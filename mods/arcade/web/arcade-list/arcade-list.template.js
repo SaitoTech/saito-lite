5ad633ae7a64e9ca81d969765ebe5e01c7778fa9
David Lancashire <david@Davids-MacBook-Pro.local>
2019-10-14 16:46:54 +0700

    cleaning up stuff

--- mods/arcade/web/arcade-list/arcade-list.template.js
+++
@@ -1,9 +0,0 @@
-export const ArcadeListTemplate = () => {
-  return `
-    <div class="arcade container">
-      <h3>Active Games</h2>
-      <div class="games-table"></div>
-    </div>
-    <button id="arcade" class="create-button"><i class="fas fa-plus"></i></button>
-  `;
-***REMOVED***
