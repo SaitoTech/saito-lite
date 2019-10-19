234c2995a6ed4131fd568b2592a76a861e746426
BearGuy <stephen@peterkins.ca>
2019-10-19 15:05:37 +0700

    small changes to email-add to fit in new email stucture

--- mods/email/lib/email-list/email-list.js
+++ mods/email/lib/email-list/email-list.js
@@ -1,13 +1,13 @@
-const EmailAdd = require('../email-add/email-add.js');
+const EmailAdd = require('../email-form/email-form.js');
 const EmailListTemplate = require('./email-list.template.js');
 const EmailListRowTemplate = require('./email-list-row.template.js');
 
 module.exports = EmailList = {
 
-    parentmod: {***REMOVED***,
+    app: {***REMOVED***,
 
     render(app) {
-***REMOVED*** if (parentmod) { this.parentmod = parentmod; ***REMOVED***
+        if (app) { this.app = app; ***REMOVED***
         document.querySelector('.email-body').innerHTML = EmailListTemplate();
 
         app.emails.forEach(mail => {
