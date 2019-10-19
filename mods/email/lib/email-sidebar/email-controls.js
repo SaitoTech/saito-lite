6417f7e6c49d0b64386c88c47678c4abe73d4d3f
David Lancashire <david@saito.tech>
2019-10-19 19:54:42 +0700

    email and delete transactions

--- mods/email/lib/email-sidebar/email-controls.js
+++ mods/email/lib/email-sidebar/email-controls.js
@@ -1,8 +1,16 @@
 
-module.exports = EmailComposeBtn = {
+module.exports = EmailControls = {
 
 
     render(app, data=[]) {
+
+      let apps_menu = document.querySelector(".email_apps");
+      if (!apps_menu) { return; }
+
+
+      for (let i = 0; i < data.length; i++) {
+	apps_menu.innerHTML += `<li class="email-navigator-item">${data[i].name}</li>`;
+      }
 
       this.attachEvents(app);
 
@@ -11,6 +19,7 @@ module.exports = EmailComposeBtn = {
 
     attachEvents(app) {
     
+
     }
 
 }
