3d0a191f20962b03a83474e4aa01b5784b74a15e
BearGuy <stephen@peterkins.ca>
2019-10-19 21:53:26 +0700

    fixed retrieving email-apps from DOM

--- mods/email/lib/email-sidebar/email-chat.js
+++ mods/email/lib/email-sidebar/email-chat.js
@@ -2,17 +2,11 @@ const EmailChatTemplate = require('./email-chat.template.js');
 
 module.exports = EmailChat = {
 
-
     render(app, data=[]) {
-
+      document.querySelector(".email-sidebar").innerHTML += EmailChatTemplate();
       this.attachEvents(app);
-
 ***REMOVED***,
 
-
-    attachEvents(app) {
-
-
-***REMOVED***
+    attachEvents(app) {***REMOVED***
 
 ***REMOVED***
