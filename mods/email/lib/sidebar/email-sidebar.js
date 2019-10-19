6cf6bf497d18e7e26c9f03de7795299ecdd4f5ca
David Lancashire <david@saito.tech>
2019-10-19 14:11:52 +0700

    sidebar UI update

--- mods/email/lib/sidebar/email-sidebar.js
+++ mods/email/lib/sidebar/email-sidebar.js
@@ -2,13 +2,8 @@ const EmailSidebarTemplate 	= require('./email-sidebar.template.js');
 
 const EmailChat 		= require('./email-chat.js');
 const EmailChatTemplate 	= require('./email-chat.template.js');
-const EmailComposeBtn 		= require('./email-compose-btn.js');
-const EmailComposeBtnTemplate 	= require('./email-compose-btn.template.js');
-const EmailLoader 		= require('./email-loader.js');
-const EmailLoaderTemplate 	= require('./email-loader.template.js');
-const EmailNavigator 		= require('./email-navigator.js');
-const EmailNavigatorTemplate 	= require('./email-navigator.template.js');
-
+const EmailControls 		= require('./email-controls.js');
+const EmailControlsTemplate 	= require('./email-controls.template.js');
 
 
 module.exports = EmailSidebar = {
@@ -18,41 +13,19 @@ module.exports = EmailSidebar = {
 
     render(app, data=[]) {
 
-      if (data.length > 0) {
-	for (let i = 0; i < data.length; i++) {
-	  if (data[i] == "chat") { components[3].render(); }
-	}
-	this.attachEvents();
-	return;
-      }
-
-
-      let email_sidebar_compose_btn = new EmailComposeBtn();
-      let email_sidebar_navigator = new EmailNavigator();
-      let email_sidebar_loader = new EmailLoader();
-      let email_sidebar_chat = new EmailChat();
-
-      components.push(email_sidebar_compose_btn);
-      components.push(email_sidebar_navigator);
-      components.push(email_sidebar_loader);
-      components.push(email_sidebar_chat);
+      components.push(new EmailControls());
+      components.push(new EmailChat());
 
       let sidebar_div = document.querySelector(".email-sidebar");
       if (!sidebar_div) { return; }
 
-      for (let i = 0; i < components.length; i++) {
-	sidebar_div.innerHTML += EmailComposeBtnTemplate();
-	sidebar_div.innerHTML += EmailNavigatorTemplate();
-	sidebar_div.innerHTML += EmailLoaderTemplate();
-	sidebar_div.innerHTML += EmailChatTemplate();
-      }
+      sidebar_div.innerHTML += EmailControlsTemplate();
+      sidebar_div.innerHTML += EmailChatTemplate();
 
-      for (let i = 0; i < components.length; i++) {
-        components[i].render(app);
-        components[i].attachEvents(app);
-      }
+      this.components[0].render(app);
+      this.components[1].render(app);
 
-      this.attachEvents();
+      this.attachEvents(app);
 
     },
 
