cf5873ba80f5d99892e871c8bd99822db24c8491
David Lancashire <david@saito.tech>
2019-10-19 20:29:45 +0700

    no problems left caused by David

--- mods/email/email.js
+++ mods/email/email.js
@@ -88,7 +88,7 @@ class Email extends ModTemplate {
 	    msg.message = "This email is not actually loaded from a remote server, but once the archives are saving transactions and returning them instead of just dummy text, we can easily correct this.";
 	    msg.timestamp = new Date().getTime();
 
-	this.emails.unshift(msg);
+	this.emails.inbox.unshift(msg);
 	EmailList.render(this);
 
 ***REMOVED***this.addEmail(msg);
@@ -130,7 +130,7 @@ class Email extends ModTemplate {
 
   addEmail(tx) {
     let {title, message***REMOVED*** = tx.returnMessage();
-    this.emails.unshift({title, message, timestamp: tx.transaction.ts***REMOVED***);
+    this.emails.inbox.unshift({title, message, timestamp: tx.transaction.ts***REMOVED***);
     if (this.app.BROWSER) { EmailList.render(this); ***REMOVED***
   ***REMOVED***
 
