0b55add28deb37edb6395aec9d525bdbe4af5e96
David Lancashire <david@saito.tech>
2019-10-19 22:31:48 +0700

    events in email controls

--- mods/email/lib/email-sidebar/email-controls.js
+++ mods/email/lib/email-sidebar/email-controls.js
@@ -6,28 +6,31 @@ module.exports = EmailControls = {
     render(app, data) {
 
       document.querySelector(".email-sidebar").innerHTML += EmailControlsTemplate();
-      document.querySelector(".email-sidebar").innerHTML += EmailChatTemplate();
 
       let email_apps = document.querySelector(".email-apps");
       for (let i = 0; i < data.mods.length; i++) {
 	email_apps.innerHTML += `<li class="email-navigator-item">${data.mods[i].name***REMOVED***</li>`;
   ***REMOVED***
-
       this.attachEvents(app);
 ***REMOVED***,
 
 
     attachEvents(app) {
-      document.querySelector('.email-navigator-item')
-      	    .addEventListener('click', (e) => {
-alert("TEST CLICK!");
-	      var elements = document.getElementsByClassName('email-navigator-active');
-	      for (let i = elements.length-1; i >= 0; i++) {
-   		elements[i].classList.remove('email-navigator-active');
-	  ***REMOVED***
+alert("EVENT ATTACHED");
+try {
+      let elements = document.getElementsByClassName('email-navigator-item')
+      Array.from(elements).forEach(elem => {
+alert("EVENT ATTACHED 2");
+        elem.addEventListener('click', (e) => {
+	  alert("TEST CLICK");
+	***REMOVED***);
+  ***REMOVED***);
 
-        ***REMOVED***);
+alert("successfully attached event listeners!");
 
+***REMOVED*** catch (err) {
+  alert("ERROR ATTACHING EVENT: " + err);
+***REMOVED***
 ***REMOVED***
 
 ***REMOVED***
