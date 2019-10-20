b62937e736cae94b41aa27feb2c8af06bb082f9c
BearGuy <stephen@peterkins.ca>
2019-10-20 22:32:51 +0700

    icon functionality in email detail

--- mods/email/lib/email-header/email-detail-header/email-detail-header.js
+++ mods/email/lib/email-header/email-detail-header/email-detail-header.js
@@ -1,8 +1,10 @@
+const EmailForm = require('../../email-form/email-form');
 const EmailDetailHeaderTemplate = require('./email-detail-header.template');
 
 module.exports = EmailDetailHeader = {
   render(app, data) {
-    document.querySelector('.email-header').innerHTML = EmailDetailHeaderTemplate(data.selected_email.transaction.msg.title);
+    let { msg ***REMOVED*** = data.selected_email.transaction;
+    document.querySelector('.email-header').innerHTML = EmailDetailHeaderTemplate(msg.title);
     this.attachEvents(app, data);
   ***REMOVED***,
 
@@ -13,6 +15,21 @@ module.exports = EmailDetailHeader = {
               data.parentmod.selected_email = {***REMOVED***;
               data.emailList.render(app, data);
               data.emailList.attachEvents(app, data);
+        ***REMOVED***);
+
+    document.getElementById('email-detail-reply')
+            .addEventListener('click', (e) => {
+              let { to ***REMOVED*** = data.selected_email.transaction;
+              EmailForm.render(app, data);
+              document.getElementById('email-to-address').value = to[0].add;
+        ***REMOVED***);
+
+    document.getElementById('email-detail-forward')
+            .addEventListener('click', (e) => {
+              let { msg ***REMOVED*** = data.selected_email.transaction;
+              EmailForm.render(app, data);
+              document.querySelector('.email-title').value = msg.title;
+              document.querySelector('.email-text').value = msg.message;
         ***REMOVED***);
   ***REMOVED***
 ***REMOVED***
