c98a91ca5818b435638606a4af8d2263fb099b6b
David Lancashire <david@saito.tech>
2019-10-21 12:01:42 +0700

    email menu responsive

--- mods/email/lib/email-main/email-body/email-detail/email-detail.js
+++ mods/email/lib/email-main/email-body/email-detail/email-detail.js
@@ -2,15 +2,16 @@ const EmailDetailHeader   = require('../../email-header/email-detail-header');
 const EmailDetailTemplate = require('./email-detail.template');
 
 module.exports = EmailDetail = {
+
   render(app, data) {
     let email_body = document.querySelector('.email-body')
-    email_body.innerHTML = EmailDetailTemplate(data.selected_email);
+    email_body.innerHTML = EmailDetailTemplate(data.parentmod.selected_email);
     console.log(email_body);
 
     EmailDetailHeader.render(app, data);
 
-    // this.attachEvents(app, data);
   },
 
-  // attachEvents(app, data) {}
+  attachEvents(app, data) {}
+
 }
