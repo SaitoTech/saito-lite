754ef774a2964096d89c1cfe6363ca9668024f64
BearGuy <stephen@peterkins.ca>
2019-10-22 19:04:38 +0700

    small css changes to appspace

--- mods/debug/lib/email-appspace/debug-appspace.js
+++ mods/debug/lib/email-appspace/debug-appspace.js
@@ -7,7 +7,6 @@ module.exports = DebugAppspace = {
       document.getElementById("email-appspace-debug").innerHTML = JSON.stringify(app.options, null, 2);
     },
 
-    attachEvents(app, data) {
-    }
+    attachEvents(app, data) {}
 
 }
