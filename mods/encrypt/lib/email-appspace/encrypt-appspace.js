c2d89b19f38ae83e41c85f979675f1a454be1cb0
David Lancashire <david@saito.tech>
2019-10-31 13:40:26 +0800

    update so lite-clients work again

--- mods/encrypt/lib/email-appspace/encrypt-appspace.js
+++ mods/encrypt/lib/email-appspace/encrypt-appspace.js
@@ -15,14 +15,6 @@ module.exports = EncryptAppspace = {
 	  data.parentmod.appspace_mod.initiate_key_exchange(recipient);
       });
 
-      document.querySelector('.fetch')
-        .addEventListener('click', (e) => {
-alert("testing fetch identifier: ");
-	  data.parentmod.app.keys.fetchIdentifier("*", function(rows) {
-alert("RESULT: " + JSON.stringify(rows));
-	  });
-      });
-
     },
 
 }
