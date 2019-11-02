0ba40c8044ac868c8594c037c55adbf138c1517d
David Lancashire <david@saito.tech>
2019-11-02 23:05:50 +0800

    Get Tokens module -- template for information about tx faucet

---
+++ mods/gettokens/lib/email-appspace/gettokens-appspace.js
@@ -0,0 +1,12 @@
+const GetTokensAppspaceTemplate 	= require('./gettokens-appspace.template.js');
+
+module.exports = GetTokensAppspace = {
+
+    render(app, data) {
+      document.querySelector(".email-appspace").innerHTML = GetTokensAppspaceTemplate();
+***REMOVED***,
+
+    attachEvents(app, data) {
+***REMOVED***,
+
+***REMOVED***
---
+++ mods/gettokens/lib/email-appspace/gettokens-appspace.template.js
@@ -0,0 +1,5 @@
+module.exports = GetTokensAppspaceTemplate = () => {
+  return `
+    This module tracks the tokens you have been given / refunded by the Transaction Faucet
+  `;
+***REMOVED***
