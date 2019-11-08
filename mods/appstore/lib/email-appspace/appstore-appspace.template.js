558404eb127d5dd0caf5d1636318fbd5f9180928
Richard Parris <github@hda0.net>
2019-11-08 11:47:27 +0100

    styling tweaks

--- mods/appstore/lib/email-appspace/appstore-appspace.template.js
+++ mods/appstore/lib/email-appspace/appstore-appspace.template.js
@@ -1,27 +1,25 @@
 module.exports = AppStoreAppspaceTemplate = () => {
   return `
-
-      <div class="email-appspace-appstore">
-
-	<h2>Submit Module to AppStore</h2>
-
+<div class="email-appspace-appstore">
+  <h2>Submit Module to AppStore</h2>
+  <div>
+    <h4>Please attach your Saito module as a ZIP file:</h4>
+  </div>
+  <fieldset>
+  <div class="grid-2">
+	<div>File:</div>
 	<div>
-
-	  Please attach your Saito module as a ZIP file:
-
-	  <p></p>
-
-	  <input type="hidden" name="module-file" id="module-file" />
+	<input type="hidden" name="module-file" id="module-file" />
           <input type="file" id="module-file-upload-btn" multiple accept="*" />
+    </div>
+  </div>
+  <button class="button inactive" id="appstore-submit">Upload to Network</button>
+  <p>
+	  <h5>Uploading a module automatically submits it to all listening AppStores on the Saito network.</h5>
+  </p>
+  </fieldset>
+</div>
+<hr />
 
-	  <p></p>
-
-	  <button class="button inactive" id="appstore-submit">Upload to Network</button>
-
-	  <p></p>
-
-	  Uploading a module automatically submits it to all listening AppStores on the Saito network.
-
-      </div>
   `;
 }
