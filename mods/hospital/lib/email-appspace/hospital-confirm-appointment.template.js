3dd4db1e2dab5ebbc31f51856783c7bac4cad263
zhangxiaoyu <zhangxiaoyu@zhangxioyudeMBP.lan>
2019-11-13 23:51:46 +0800

    css and layout changes to hospital

--- mods/hospital/lib/email-appspace/hospital-confirm-appointment.template.js
+++ mods/hospital/lib/email-appspace/hospital-confirm-appointment.template.js
@@ -3,32 +3,26 @@ module.exports = HospitalConfirmAppointmentTemplate = () => {
   <link rel="stylesheet" href="/hospital/css/email-appspace.css">
 
   <h3>Confirm Appointment</h3>
-
-  <div class="appointment-info">
-    You have selected an appointment at:
-
+  <div class= "appointment-info-box">
+   <div class="appointment-info">
     <p></p>
+    <p><strong>Hospital:</strong>&nbsp;Sister of Mercy Childrens' Hospital</p>
+    <p><strong>Time:</strong>&nbsp;9:30 AM on Wednesday, </p>
+    <p><strong>Date:</strong>&nbsp;10th January 2019</p>
+    <p><strong>Location:</strong>&nbsp;5729 Silversmith Road, Petaluma, CA581-403-2938</p>
+    <p><strong>Note:</strong>&nbsp;drink a plenty of water before blood check,Please</p>
+    </div>
+  </div>
 
-    Sister of Mercy Childrens' Hospital
-    9:30 AM on Wednesday, January 2nd (tomorrow)
-    5729 Silversmith Road, Petaluma, CA
-
-    581-403-2938
-
-    <h4>Please confirm your 
-
-
+    <h4 class=confirmInfo>Please confirm your 
     N.B.: Clicking submit makes the booking request into the system. If you have
     used this system before, bookings are usually automatic -- you will receive
     confirmation within a few minutes.
 
-
-    <b>Is this information correct?</by>    
-
-    <button class="reselect">resel</button>
-    <button class="confirm super">confirm</button>
-
-  </div>
-
-    `;
+    <b>Is this information correct?</by>  
+    </h4>  
+    <button class="hospital-btn reselect">resel</button>
+    <button class="confirm-btn super" id="confirm-btn">confirm</button>
+  
+    `
 ***REMOVED***
