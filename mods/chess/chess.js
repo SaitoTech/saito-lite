92cb79e4ed1884e0190fbe00f960c46cc5551990
BearGuy <stephen@peterkins.ca>
2019-11-17 21:43:59 +0800

    get chess working

--- mods/chess/chess.js
+++ mods/chess/chess.js
@@ -3,35 +3,35 @@ const GameTemplate = require('../../lib/templates/gametemplate');
 var this_chess = null;
 var chess = null;
 var chessboard = null;
-  
-  
-  
+
+
+
 //////////////////
 // CONSTRUCTOR  //
 //////////////////
 class Chessgame extends GameTemplate {
-  
+
   constructor(app) {
-  
+
     super(app);
-  
+
     this.publickey = app.wallet.returnPublicKey();
-  
+
     this.name = "Chess";
     this.description = "Chess is a two-player strategy board game played on a checkered board with 64 squares arranged in an 8×8 grid."
-  
+
     this.board = null;
     this.engine = null;
     this_chess = this;
-  
+
     return this;
-  
+
   ***REMOVED***
-  
-  
+
+
 
   async initializeGame(game_id) {
-  
+
     console.log('######################################################');
     console.log('######################################################');
     console.log('######################         #######################');
@@ -41,34 +41,32 @@ class Chessgame extends GameTemplate {
     console.log('######################################################');
 
     if (this.browser_active == 1) {
-  
+
       // enable chat
       //if (!this.app.browser.isMobileBrowser(navigator.userAgent)) {
       //  const chat = this.app.modules.returnModule("Chat");
       //  chat.addPopUpChat();
       //***REMOVED***
-  
-      chess = require('chess.js');
 
+      let chess = require('chess.js');
+      let chessboard = require("./www/js/chessboard");
 
-
-      chessboard = require(("web/chessboard"));
       this.board = new chessboard('board', { pieceTheme: 'chess/pieces/{piece***REMOVED***.png' ***REMOVED***);
       this.engine = new chess.Chess();
 ***REMOVED***
-  
+
     //
     // load this.game object
     //
     this.loadGame(game_id);
-  
+
     //
     // finish initializing
     //
     if (this.game.initializing == 1) {
       this.game.queue.push("READY");
 ***REMOVED***
-  
+
 
     if (this.browser_active == 1) {
       if (this.game.position != undefined) {
@@ -76,15 +74,15 @@ class Chessgame extends GameTemplate {
   ***REMOVED*** else {
         this.game.position = this.engine.fen();
   ***REMOVED***
-  
+
       if (this.game.target == this.game.player) {
         this.setBoard(this.engine.fen());
   ***REMOVED*** else {
         this.lockBoard(this.engine.fen());
   ***REMOVED***
-  
+
       var opponent = this.game.opponents[0];
-  
+
       if (this.app.crypto.isPublicKey(opponent)) {
         if (this.app.keys.returnIdentifierByPublicKey(opponent).length >= 6) {
           opponent = this.app.keys.returnIdentifierByPublicKey(opponent);
@@ -98,23 +96,23 @@ class Chessgame extends GameTemplate {
       ***REMOVED***
     ***REMOVED***
   ***REMOVED***
-  
+
       $('#opponent_id').html(opponent);
       this.updateStatusMessage();
       this.attachEvents();
-  
+
 ***REMOVED***
-  
+
   ***REMOVED***
-  
-  
-  
-  
+
+
+
+
   ////////////////
   // handleGame //
   ////////////////
   handleGameLoop(msg={***REMOVED***) {
-  
+
     msg = {***REMOVED***;
 console.log("QUEUE: " + this.game.queue);
     if (this.game.queue.length > 0) {
@@ -123,7 +121,7 @@ console.log("QUEUE: " + this.game.queue);
       msg.extra = {***REMOVED***;
 ***REMOVED***
     this.game.queue.splice(this.game.queue.length-1, 1);
-  
+
     if (msg.extra == undefined) {
       console.log("NO MESSAGE DEFINED!");
       return;
@@ -132,7 +130,7 @@ console.log("QUEUE: " + this.game.queue);
       console.log("NO MESSAGE RECEIVED!");
       return;
 ***REMOVED***
-  
+
     //
     // the message we get from the other player
     // tells us the new board state, so we
@@ -142,7 +140,7 @@ console.log("QUEUE: " + this.game.queue);
     let data = JSON.parse(msg.extra.data);
     this.game.position = data.position;
     this.game.target = msg.extra.target;
-  
+
     if (msg.extra.target == this.game.player) {
       if (this.browser_active == 1) {
         this.setBoard(this.game.position);
@@ -154,42 +152,42 @@ console.log("QUEUE: " + this.game.queue);
         this.lockBoard(this.game.position);
   ***REMOVED***
 ***REMOVED***
-  
+
     this.saveGame(this.game.id);
-  
+
     return 0;
-  
+
   ***REMOVED***
-  
-  
-  
+
+
+
   endTurn(data) {
-  
+
     let extra = {***REMOVED***;
-  
+
     extra.target = this.returnNextPlayer(this.game.player);
     extra.data = JSON.stringify(data);
-  
+
     let data_to_send = this.app.crypto.stringToBase64(JSON.stringify(extra));
     this.game.turn = [data_to_send];
     this.moves = [];
     this.sendMessage("game", extra);
-  
+
   ***REMOVED***
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
   attachEvents() {
-  
+
     $('#move_accept').off();
     $('#move_accept').on('click', function () {
-  
+
       console.log('send move transaction and wait for reply.');
-  
+
       var data = {***REMOVED***;
       data.white = this_chess.game.white;
       data.black = this_chess.game.black;
@@ -197,39 +195,39 @@ console.log("QUEUE: " + this.game.queue);
       data.position = this_chess.engine.fen();
       data.move = this_chess.game.move;
       this_chess.endTurn(data);
-  
+
       $('#move_accept').prop('disabled', true);
       $('#move_accept').removeClass('green');
-  
+
       $('#move_reject').prop('disabled', true);
       $('#move_reject').removeClass('red');
-  
+
 ***REMOVED***);
-  
-  
+
+
     $('#move_reject').off();
     $('#move_reject').on('click', function () {
-  
+
       this_chess.setBoard(this_chess.game.position);
       $('#move_accept').prop('disabled', true);
       $('#move_accept').removeClass('green');
-  
+
       $('#move_reject').prop('disabled', true);
       $('#move_reject').removeClass('red');
-  
+
 ***REMOVED***);
-  
+
     $(window).resize(function () {
       this_chess.board.resize();
 ***REMOVED***);
   ***REMOVED***
-  
-  
-  
+
+
+
   updateStatusMessage(str = "") {
-  
+
     if (this.browser_active != 1) { return; ***REMOVED***
-  
+
     //
     // print message if provided
     //
@@ -238,37 +236,37 @@ console.log("QUEUE: " + this.game.queue);
       statusEl.html(str);
       return;
 ***REMOVED***
-  
+
     var status = '';
-  
+
     var moveColor = 'White';
     if (this.engine.turn() === 'b') {
       moveColor = 'Black';
 ***REMOVED***
-  
+
     // checkmate?
     if (this.engine.in_checkmate() === true) {
       status = 'Game over, ' + moveColor + ' is in checkmate.';
 ***REMOVED***
-  
+
     // draw?
     else if (this.engine.in_draw() === true) {
       status = 'Game over, drawn position';
 ***REMOVED***
-  
+
     // game still on
     else {
-  
+
       status = moveColor + ' to move';
-  
+
       // check?
       if (this.engine.in_check() === true) {
         status += ', ' + moveColor + ' is in check';
   ***REMOVED***
-  
+
 ***REMOVED***
-  
-  
+
+
     var statusEl = $('#status');
     statusEl.html(status);
     var capturedEL = $('#captured');
@@ -279,22 +277,22 @@ console.log("QUEUE: " + this.game.queue);
     //capturedEL.html(this.game.captured.white + "</br>" + this.game.captured.black);
     capturedEL.html(this.returnCapturedHTML(this.returnCaptured(this.engine.fen())));
     this.updateLog();
-  
+
   ***REMOVED***;
-  
-  
-  
-  
+
+
+
+
   setBoard(position) {
-  
+
     this.game.moveStartPosition = position;
-  
+
     if (this.board != undefined) {
       if (this.board.destroy != undefined) {
         this.board.destroy();
   ***REMOVED***
 ***REMOVED***
-  
+
     let cfg = {
       draggable: true,
       position: position,
@@ -307,65 +305,65 @@ console.log("QUEUE: " + this.game.queue);
       onMoveEnd: this.onMoveEnd,
       onChange: this.onChange
 ***REMOVED***;
-  
+
     if (this.browser_active == 1) {
       this.board = new chessboard('board', cfg);
 ***REMOVED***
     this.engine.load(position);
-  
+
     if (this.game.player == 2 && this.browser_active == 1) {
       this.board.orientation('black');
 ***REMOVED***
-  
+
   ***REMOVED***
-  
-  
+
+
   lockBoard(position) {
-  
+
     if (this.board != undefined) {
       if (this.board.destroy != undefined) {
         this.board.destroy();
   ***REMOVED***
 ***REMOVED***
-  
+
     let cfg = {
       pieceTheme: 'chess/pieces/{piece***REMOVED***.png',
       moveSpeed: 0,
       position: position
 ***REMOVED***
-  
+
     this.board = new chessboard('board', cfg);
     this.engine.load(position);
-  
+
     if (this.game.player == 2) {
       this.board.orientation('black');
 ***REMOVED***
-  
+
   ***REMOVED***
-  
+
   //////////////////
   // Board Config //
   //////////////////
   onDragStart(source, piece, position, orientation) {
-  
+
     if (this_chess.engine.game_over() === true ||
       (this_chess.engine.turn() === 'w' && piece.search(/^b/) !== -1) ||
       (this_chess.engine.turn() === 'b' && piece.search(/^w/) !== -1)) {
       return false;
 ***REMOVED***
   ***REMOVED***;
-  
+
 
 
 
   onDrop(source, target, piece, newPos, oldPos, orientation, topromote) {
-  
+
     this_chess.removeGreySquares();
-  
+
     this_chess.game.move = this_chess.engine.fen().split(" ").slice(-1)[0] + " " + this_chess.colours(this_chess.engine.fen().split(" ")[1]) + ": ";
-  
+
     //was a pawn moved to the last rank
-    if ((source.charAt(1) == 7 && target.charAt(1) == 8 && piece == 'wP') 
+    if ((source.charAt(1) == 7 && target.charAt(1) == 8 && piece == 'wP')
         || (source.charAt(1) == 2 && target.charAt(1) == 1 && piece == 'bP')) {
       // check with user on desired piece to promote.
       this_chess.checkPromotion(source, target, piece.charAt(0));
@@ -379,9 +377,9 @@ console.log("QUEUE: " + this.game.queue);
       // illegal move
       if (move === null) return 'snapback';
       // legal move - make it
-  
+
       this_chess.game.move += this_chess.pieces(move.piece) + " ";
-  
+
       this_chess.game.move += " - " + move.san;
 ***REMOVED***
   ***REMOVED***
@@ -397,19 +395,19 @@ console.log("QUEUE: " + this.game.queue);
 ***REMOVED***);
     $('#promotion').hide();
     $('#buttons').show();
-  
+
     this_chess.updateStatusMessage("Confirm Move to Send!");
-  
+
     // legal move - make it
-  
+
     this_chess.game.move += this_chess.pieces(move.piece) + " ";
-  
+
     this_chess.game.move += " - " + move.san;
-  
+
     this_chess.updateStatusMessage('Pawn promoted to ' + this_chess.pieces(piece) + '.');
-  
+
   ***REMOVED***;
-  
+
 
 
 
@@ -439,38 +437,38 @@ console.log("QUEUE: " + this.game.queue);
 
 
   onMouseoverSquare(square, piece) {
-  
+
     // get list of possible moves for this square
     var moves = this_chess.engine.moves({
       square: square,
       verbose: true
 ***REMOVED***);
-  
+
     // exit if there are no moves available for this square
     if (moves.length === 0) { return; ***REMOVED***
-  
+
     // highlight the square they moused over
     this_chess.greySquare(square);
-  
+
     // highlight the possible squares for this piece
     for (var i = 0; i < moves.length; i++) {
       this_chess.greySquare(moves[i].to);
 ***REMOVED***
   ***REMOVED***;
-  
+
 
 
 
   onMouseoutSquare(square, piece) {
     this_chess.removeGreySquares();
   ***REMOVED***;
-  
+
 
 
   onSnapEnd() {
     this_chess.board.position(this_chess.engine.fen());
   ***REMOVED***;
-  
+
 
 
   removeGreySquares() {
@@ -480,55 +478,55 @@ console.log("QUEUE: " + this.game.queue);
 
 
   greySquare(square) {
-  
+
     var squareEl = $('#board .square-' + square);
-  
+
     var background = '#a9a9a9';
     if (squareEl.hasClass('black-3c85d') === true) {
       background = '#696969';
 ***REMOVED***
-  
+
     squareEl.css('background', background);
-  
+
   ***REMOVED***;
-  
-  
+
+
 
 
   onChange(oldPos, newPos) {
-  
+
     this_chess.lockBoard(this_chess.engine.fen(newPos));
-  
+
     $('#move_accept').prop('disabled', false);
     $('#move_accept').addClass('green');
-  
+
     $('#move_reject').prop('disabled', false);
     $('#move_reject').addClass('red');
-    
+
     if ($('#buttons').is("vissible")) {
       this_chess.updateStatusMessage("Confirm Move to Send!");
 ***REMOVED***
-  
+
   ***REMOVED***;
 
 
-  
+
   colours(x) {
-  
+
     switch (x) {
       case "w": return ("White");
       case "b": return ("Black");
 ***REMOVED***
-  
+
     return;
-  
+
   ***REMOVED***
-  
+
 
 
 
   pieces(x) {
-  
+
     switch (x) {
       case "p": return ("Pawn");
       case "r": return ("Rook");
@@ -537,11 +535,11 @@ console.log("QUEUE: " + this.game.queue);
       case "q": return ("Queen");
       case "k": return ("King");
 ***REMOVED***
-  
+
     return;
-  
+
   ***REMOVED***
-  
+
 
 
 
@@ -565,7 +563,7 @@ console.log("QUEUE: " + this.game.queue);
   ***REMOVED***
 
 
-  
+
   returnCapturedHTML(acapt) {
     let captHTML = "";
     for (var i = 0; i < acapt[0].length; i++) {
@@ -585,23 +583,23 @@ console.log("QUEUE: " + this.game.queue);
     var pieceImg = '<img class="captured" alt="' + p + '" src = "/chess/pieces/' + c + p.toUpperCase() + '.png">';
     return (pieceImg);
   ***REMOVED***
-  
+
 
 
 
   returnGameOptionsHTML() {
-  
+
     return `
           <h3>Chess: </h3>
-  
+
           <form id="options" class="options">
-  
+
             <label for="color">Pick Your Color:</label>
             <select name="color">
               <option value="black" default>Black</option>
               <option value="white">White</option>
             </select>
-  
+
             </form>
             `;
   ***REMOVED***
