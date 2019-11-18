661c19be7b4e31ada1c498baf49ddaa9e4e9a015
zhangxiaoyu <zhangxiaoyu@zhangxioyudeMBP.lan>
2019-11-18 18:55:52 +0800

    view appointments updtated

--- mods/hospital/lib/email-appspace/hospital-appointment-qrcode.template.js
+++ mods/hospital/lib/email-appspace/hospital-appointment-qrcode.template.js
@@ -1,7 +1,18 @@
 module.exports = HospitalAppointmentProfileTemplate = () => {
   return `
+<link rel="stylesheet" href="/hospital/css/email-appspace.css">
+    <div class="qrcode-appointment">
+    <div id="qrcode"></div>
+     
+    <div id="qrcode-appointment-info">
+        <div class="appointment-hospital-name">Sister of Mercy Childrens Hospital </div>
+        <div class="appointment-hospital-address">Silversmith Road,Petaluma,CA 58-403-2938</div>
+        <div class="appointment-hospital-time">9:30 AM - Wednesday, January 2 (2019)</div>
+    </div>
 
-    THIS IS THE QR CODE
+    </div>
 
-    `;
+
+        `;
 ***REMOVED***
+
--- mods/hospital/lib/email-appspace/hospital-view-appointment.template.js
+++ mods/hospital/lib/email-appspace/hospital-view-appointment.template.js
@@ -4,8 +4,34 @@ module.exports = HospitalViewAppointmentTemplate = () => {
 
   <h3>Your Scheduled Appointments</h3>
 
-  <div class="appointments">
-  </div>
+  <ul class="view-appointments-box">
 
+     <li class="appointment" id="">
+      <div class="appointment-hospital-name">Sister of Mercy Childrens' Hospital</div>
+      <div class="appointment-hospital-address">Silversmith Road,Petaluma,CA 58-403-2938</div>
+      <div class="appointment-hospital-time">9:30 AM - Wednesday, January 2 (2019)</div>
+    </li>
+
+    <li class="appointment" id="">
+      <div class="appointment-hospital-name">Sister of Mercy Childrens' Hospital</div>
+      <div class="appointment-hospital-address">Silversmith Road,Petaluma,CA 58-403-2938</div>
+      <div class="appointment-hospital-time">9:30 AM - Wednesday, January 2 (2019)</div>
+    </li>
+
+    <li class="appointment" id="">
+      <div class="appointment-hospital-name">Sister of Mercy Childrens' Hospital</div>
+      <div class="appointment-hospital-address">Silversmith Road,Petaluma,CA 58-403-2938</div>
+      <div class="appointment-hospital-time">9:30 AM - Wednesday, January 2 (2019)</div>
+    </li>
+
+    <li class="appointment" id="">
+      <div class="appointment-hospital-name">Sister of Mercy Childrens' Hospital</div>
+      <div class="appointment-hospital-address">Silversmith Road,Petaluma,CA 58-403-2938</div>
+      <div class="appointment-hospital-time">9:30 AM - Wednesday, January 2 (2019)</div>
+    </li>
+
+    
+
+  </ul>
     `;
 ***REMOVED***
