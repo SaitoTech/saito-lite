ae344e5c38277b72a4a0e8de88bded8b199b18f1
Richard Parris <github@hda0.net>
2019-11-23 00:52:46 +0800

    website module messy start

---
+++ mods/website/website.js
@@ -0,0 +1,45 @@
+var saito = require('../../lib/saito/saito');
+var ModTemplate = require('../../lib/templates/modtemplate');
+const Header = require('../../lib/ui/header/header');
+const HeaderMenu = require('./lib/header/website-header-menu')
+
+
+class Website extends ModTemplate {
+
+  constructor(app) {
+    super(app);
+
+    this.app            = app;
+    this.name           = "Website";
+
+    return this;
+  ***REMOVED***
+
+  initializeHTML(app) {
+
+    super.initializeHTML(app);
+
+    Header.render(app, this.uidata);
+    Header.attachEvents(app, this.uidata);
+    HeaderMenu.render(app);
+
+       // Use for Carousel
+       importGlide = async () => {
+        const Glide = await import('../../lib/helpers/glide.min.js');
+        this.glide = new Glide.default('.glide', {
+          type: 'carousel',
+          autoplay: 3000,
+          perView,
+    ***REMOVED***);
+  
+        this.glide.mount();
+  ***REMOVED***
+      importGlide();
+
+  ***REMOVED***
+
+
+***REMOVED***
+
+
+module.exports = Website;
