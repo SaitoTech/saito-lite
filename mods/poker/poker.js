37f5f02bdea4abf62146f5118d04368543c84228
BearGuy <stephen@peterkins.ca>
2019-11-22 17:20:21 +0800

    moved cards over to cardfan

--- mods/poker/poker.js
+++ mods/poker/poker.js
@@ -21,6 +21,8 @@ class Poker extends GameTemplate {
 
     this.hud = new GameHud(this.app, {}, {height: 200, width: 125});
 
+    this.card_img_dir = '/poker/img/cards';
+
     return this;
 
   }
@@ -93,10 +95,47 @@ class Poker extends GameTemplate {
       let mv = this.game.queue[qe].split("\t");
       let shd_continue = 1;
 
-      if (mv[0] === "notify") {
-        this.updateLog(mv[1]);
-        this.game.queue.splice(qe, 1);
+      switch (mv[0]) {
+        case "notify":
+          this.updateLog(mv[1]);
+          this.game.queue.splice(qe, 1);
+          break;
+
+        case "turn":
+          this.displayBoard();
+          this.game.queue.splice(qe, 1);
+          if (parseInt(mv[1]) == this.game.player) this.playerTurn();
+          shd_continue = 0;
+          break;
+
+        case "round":
+          this.displayBoard();
+          this.updateStatus("Your opponent is making the first move.");
+          for (let i = 0; i < this.game.opponents.length+1; i++) { this.game.queue.push("turn\t"+(i+1)); }
+          shd_continue = 1;
+          break;
+
+        case "play":
+          this.game.queue.splice(qe, 1);
+          break;
+
+        case "call":
+          this.game.queue.splice(qe, 1);
+          break;
+
+        case "fold":
+          this.game.queue.splice(qe, 1);
+          break;
+
+        case "raise":
+          this.game.queue.splice(qe, 1);
+          break;
       }
+
+      // if (mv[0] === "notify") {
+      //   this.updateLog(mv[1]);
+      //   this.game.queue.splice(qe, 1);
+      // }
 
       //
       // round
@@ -105,32 +144,32 @@ class Poker extends GameTemplate {
       // fold
       // raise
       //
-      if (mv[0] === "turn") {
-	this.displayBoard();
-        this.game.queue.splice(qe, 1);
-        if (parseInt(mv[1]) == this.game.player) {
-          this.playerTurn();
-        }
-        shd_continue = 0;
-      }
-      if (mv[0] === "round") {
-        this.displayBoard();
-        this.updateStatus("Your opponent is making the first move.");
-        for (let i = 0; i < this.game.opponents.length+1; i++) { this.game.queue.push("turn\t"+(i+1)); }
-        shd_continue = 1;
-      }
-      if (mv[0] === "play") {
-        this.game.queue.splice(qe, 1);
-      }
-      if (mv[0] === "call") {
-        this.game.queue.splice(qe, 1);
-      }
-      if (mv[0] === "fold") {
-        this.game.queue.splice(qe, 1);
-      }
-      if (mv[0] === "raise") {
-        this.game.queue.splice(qe, 1);
-      }
+  //     if (mv[0] === "turn") {
+	// this.displayBoard();
+  //       this.game.queue.splice(qe, 1);
+  //       if (parseInt(mv[1]) == this.game.player) {
+  //         this.playerTurn();
+  //       }
+  //       shd_continue = 0;
+  //     }
+  //     if (mv[0] === "round") {
+  //       this.displayBoard();
+  //       this.updateStatus("Your opponent is making the first move.");
+  //       for (let i = 0; i < this.game.opponents.length+1; i++) { this.game.queue.push("turn\t"+(i+1)); }
+  //       shd_continue = 1;
+  //     }
+  //     if (mv[0] === "play") {
+  //       this.game.queue.splice(qe, 1);
+  //     }
+  //     if (mv[0] === "call") {
+  //       this.game.queue.splice(qe, 1);
+  //     }
+  //     if (mv[0] === "fold") {
+  //       this.game.queue.splice(qe, 1);
+  //     }
+  //     if (mv[0] === "raise") {
+  //       this.game.queue.splice(qe, 1);
+  //     }
 
       //
       // avoid infinite loops
@@ -201,58 +240,58 @@ class Poker extends GameTemplate {
 
     var deck = {};
 
-    deck['1']                 = { name : "S1" }
-    deck['2']                 = { name : "S2" }
-    deck['3']                 = { name : "S3" }
-    deck['4']                 = { name : "S4" }
-    deck['5']                 = { name : "S5" }
-    deck['6']                 = { name : "S6" }
-    deck['7']                 = { name : "S7" }
-    deck['8']                 = { name : "S8" }
-    deck['9']                 = { name : "S9" }
-    deck['10']                = { name : "S10" }
-    deck['11']                = { name : "S11" }
-    deck['12']                = { name : "S12" }
-    deck['13']                = { name : "S13" }
-    deck['14']                = { name : "C1" }
-    deck['15']                = { name : "C2" }
-    deck['16']                = { name : "C3" }
-    deck['17']                = { name : "C4" }
-    deck['18']                = { name : "C5" }
-    deck['19']                = { name : "C6" }
-    deck['20']                = { name : "C7" }
-    deck['21']                = { name : "C8" }
-    deck['22']                = { name : "C9" }
-    deck['23']                = { name : "C10" }
-    deck['24']                = { name : "C11" }
-    deck['25']                = { name : "C12" }
-    deck['26']                = { name : "C13" }
-    deck['27']                = { name : "H1" }
-    deck['28']                = { name : "H2" }
-    deck['29']                = { name : "H3" }
-    deck['30']                = { name : "H4" }
-    deck['31']                = { name : "H5" }
-    deck['32']                = { name : "H6" }
-    deck['33']                = { name : "H7" }
-    deck['34']                = { name : "H8" }
-    deck['35']                = { name : "H9" }
-    deck['36']                = { name : "H10" }
-    deck['37']                = { name : "H11" }
-    deck['38']                = { name : "H12" }
-    deck['39']                = { name : "H13" }
-    deck['40']                = { name : "D1" }
-    deck['41']                = { name : "D2" }
-    deck['42']                = { name : "D3" }
-    deck['43']                = { name : "D4" }
-    deck['44']                = { name : "D5" }
-    deck['45']                = { name : "D6" }
-    deck['46']                = { name : "D7" }
-    deck['47']                = { name : "D8" }
-    deck['48']                = { name : "D9" }
-    deck['49']                = { name : "D10" }
-    deck['50']                = { name : "D11" }
-    deck['51']                = { name : "D12" }
-    deck['52']                = { name : "D13" }
+    deck['1']                 = { name : "S1.png" }
+    deck['2']                 = { name : "S2.png" }
+    deck['3']                 = { name : "S3.png" }
+    deck['4']                 = { name : "S4.png" }
+    deck['5']                 = { name : "S5.png" }
+    deck['6']                 = { name : "S6.png" }
+    deck['7']                 = { name : "S7.png" }
+    deck['8']                 = { name : "S8.png" }
+    deck['9']                 = { name : "S9.png" }
+    deck['10']                = { name : "S10.png" }
+    deck['11']                = { name : "S11.png" }
+    deck['12']                = { name : "S12.png" }
+    deck['13']                = { name : "S13.png" }
+    deck['14']                = { name : "C1.png" }
+    deck['15']                = { name : "C2.png" }
+    deck['16']                = { name : "C3.png" }
+    deck['17']                = { name : "C4.png" }
+    deck['18']                = { name : "C5.png" }
+    deck['19']                = { name : "C6.png" }
+    deck['20']                = { name : "C7.png" }
+    deck['21']                = { name : "C8.png" }
+    deck['22']                = { name : "C9.png" }
+    deck['23']                = { name : "C10.png" }
+    deck['24']                = { name : "C11.png" }
+    deck['25']                = { name : "C12.png" }
+    deck['26']                = { name : "C13.png" }
+    deck['27']                = { name : "H1.png" }
+    deck['28']                = { name : "H2.png" }
+    deck['29']                = { name : "H3.png" }
+    deck['30']                = { name : "H4.png" }
+    deck['31']                = { name : "H5.png" }
+    deck['32']                = { name : "H6.png" }
+    deck['33']                = { name : "H7.png" }
+    deck['34']                = { name : "H8.png" }
+    deck['35']                = { name : "H9.png" }
+    deck['36']                = { name : "H10.png" }
+    deck['37']                = { name : "H11.png" }
+    deck['38']                = { name : "H12.png" }
+    deck['39']                = { name : "H13.png" }
+    deck['40']                = { name : "D1.png" }
+    deck['41']                = { name : "D2.png" }
+    deck['42']                = { name : "D3.png" }
+    deck['43']                = { name : "D4.png" }
+    deck['44']                = { name : "D5.png" }
+    deck['45']                = { name : "D6.png" }
+    deck['46']                = { name : "D7.png" }
+    deck['47']                = { name : "D8.png" }
+    deck['48']                = { name : "D9.png" }
+    deck['49']                = { name : "D10.png" }
+    deck['50']                = { name : "D11.png" }
+    deck['51']                = { name : "D12.png" }
+    deck['52']                = { name : "D13.png" }
 
     return deck;
 
@@ -263,14 +302,18 @@ class Poker extends GameTemplate {
 
   displayHand() {
 
-    $('#hand').empty();
+    this.cardfan.render(this.app, this);
+    this.cardfan.attachEvents(this.app, this);
+    //.deck[0].cards[this.game.deck[0].hand[i]]);
 
-    for (let i = 0; i < this.game.deck[0].hand.length; i++) {
-      let card = this.game.deck[0].cards[this.game.deck[0].hand[i]];
-      let card_img = card.name + ".png";
-      let html = '<img class="card" src="/poker/img/cards/'+card_img+'" />';
-      $('#hand').append(html);
-    }
+    // $('#hand').empty();
+
+    // for (let i = 0; i < this.game.deck[0].hand.length; i++) {
+    //   let card = this.game.deck[0].cards[this.game.deck[0].hand[i]];
+    //   let card_img = card.name + ".png";
+    //   let html = '<img class="card" src="/poker/img/cards/'+card_img+'" />';
+    //   $('#hand').append(html);
+    // }
 
   }
 
@@ -284,10 +327,12 @@ class Poker extends GameTemplate {
     $('#deal').empty();
 
     for (let i = 0; i < 5 || i < this.game.pool[0].hand.length; i++) {
-      let card = "";
-      if (i < this.game.pool[0].hand.length) { card = this.game.pool[0].cards[this.game.pool[0].hand[i]]; } else { card = {}; card.name = "red_back"; }
-      let card_img = card.name + ".png";
-      let html = '<img class="card" src="/poker/img/cards/'+card_img+'" />';
+      let card = {};
+      if (i < this.game.pool[0].hand.length) { card = this.game.pool[0].cards[this.game.pool[0].hand[i]]; } else { card.name = "red_back.png"; }
+
+      // let card_img = card.name + ".png";
+      let html = `<img class="card" src="${this.card_img_dir}/${card.name}">`;
+      //document.getElementById('deal').innerHTML += html;
       $('#deal').append(html);
     }
 
