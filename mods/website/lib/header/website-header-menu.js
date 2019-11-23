803feb7732409da116c709fe3689d143a88cb99c
David Lancashire <david@saito.tech>
2019-11-24 00:39:47 +0700

    initial website simplify frontend

--- mods/website/lib/header/website-header-menu.js
+++
@@ -1,9 +0,0 @@
-//const sitepages = require("../../web/pages")
-const WebsiteHeaderMenuTemplate = require('./website-header-menu.template')
-
-module.exports = HeaderMenu = {
-    render(app) {
-        let header_menu = document.querySelector("#header-menu");
-        header_menu.innerHTML = WebsiteHeaderMenuTemplate();
-***REMOVED***
-***REMOVED***;
