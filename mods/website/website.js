4bcad1349f6115fd764a18a80d1c58bf78387e2e
David Lancashire <david@saito.tech>
2019-11-24 00:39:47 +0700

    initial website simplify frontend

--- mods/website/website.js
+++ mods/website/website.js
@@ -1,7 +1,6 @@
 var saito = require('../../lib/saito/saito');
 var ModTemplate = require('../../lib/templates/modtemplate');
 const Header = require('../../lib/ui/header/header');
-const HeaderMenu = require('./lib/header/website-header-menu')
 
 
 class Website extends ModTemplate {
@@ -15,29 +14,30 @@ class Website extends ModTemplate {
     return this;
   }
 
+
   initializeHTML(app) {
 
     super.initializeHTML(app);
 
     Header.render(app, this.uidata);
     Header.attachEvents(app, this.uidata);
+
+/****
     HeaderMenu.render(app);
 
-       // Use for Carousel
-       importGlide = async () => {
-        const Glide = await import('../../lib/helpers/glide.min.js');
-        this.glide = new Glide.default('.glide', {
-          type: 'carousel',
-          autoplay: 3000,
-          perView,
-        });
-  
-        this.glide.mount();
-      }
-      importGlide();
-
+    // Use for Carousel
+   importGlide = async () => {
+     const Glide = await import('../../lib/helpers/glide.min.js');
+     this.glide = new Glide.default('.glide', {
+        type: 'carousel',
+        autoplay: 3000,
+        perView,
+      });
+      this.glide.mount();
+    } 
+    importGlide();
+****/
   }
-
 
 }
 
