e7b546f82c4b1eefc9ce604edf6aa72a151442e4
Richard Parris <github@hda0.net>
2019-11-23 11:30:23 +0800

    wrapped glide import in window check so it does not break node

--- mods/arcade/arcade.js
+++ mods/arcade/arcade.js
@@ -186,18 +186,19 @@ console.log("ERROR 418019: error fetching game for observer mode");
     let perView = this.app.browser.isMobileBrowser(navigator.userAgent) ? 1 : 3;
 
     // Use for Carousel
-    importGlide = async () => {
-      const Glide = await import('./lib/glide/glide.min.js');
-      this.glide = new Glide.default('.glide', {
-        type: 'carousel',
-        autoplay: 3000,
-        perView,
-      });
+    if (typeof window !== "undefined") {
+      importGlide = async () => {
+        const Glide = await import('./lib/glide/glide.min.js');
+        this.glide = new Glide.default('.glide', {
+          type: 'carousel',
+          autoplay: 3000,
+          perView,
+        });
 
-      this.glide.mount();
+        this.glide.mount();
+      }
+      importGlide();
     }
-    importGlide();
-
   }
 
 
