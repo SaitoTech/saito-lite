4b5756f80585a96b8db746c2cdd90e4444a809fa
BearGuy <stephen@peterkins.ca>
2019-11-28 19:03:30 +0700

    purge alaunius

--- mods/alaunius/lib/alaunius-main/alaunius-header/alaunius-appspace-header/alaunius-appspace-header.template.js
+++
@@ -1,9 +0,0 @@
-module.exports = AlauniusAppspaceHeaderTempkate = (app, data) => {
-  return `
-    <div class="alaunius-detail-left-options">
-      <i id="alaunius-form-back-button" class="icon-med fas fa-arrow-left"></i>
-      <span class="alaunius-header-title">${data.parentmod.header_title***REMOVED***</span>
-    </div>
-    <div class="alaunius-balance">${app.wallet.returnBalance()***REMOVED*** Saito</div>
-  `;
-***REMOVED***
