b32c0995408cfd0a2789bb0cbc8bc7c35c9a556e
BearGuy <stephen@peterkins.ca>
2019-11-29 15:14:20 +0700

    css changes to appstore to make lists more dynamic

--- mods/appstore/lib/email-appspace/appstore-appspace.js
+++ mods/appstore/lib/email-appspace/appstore-appspace.js
@@ -4,7 +4,9 @@ const AppStoreAppspacePublish   = require('./appstore-appspace-publish/appstore-
 module.exports = AooStoreAppspace = {
 
     render(app, data) {
-      document.querySelector(".email-appspace").innerHTML = AppStoreAppspaceTemplate();
+      setTimeout(() => {
+        document.querySelector(".email-appspace").innerHTML = AppStoreAppspaceTemplate();
+      }, 2000);
     },
 
     attachEvents(app, data) {
@@ -14,44 +16,6 @@ module.exports = AooStoreAppspace = {
                 AppStoreAppspacePublish.render(app, data);
                 AppStoreAppspacePublish.attachEvents(app, data);
               }
-
-        /*
-      let fileUpload 	= document.querySelector('#module-file-upload-btn');
-      let fileObj	= document.getElementById("module-file");
-
-      fileUpload.onchange = function() {
-        var reader = new FileReader();
-        reader.onload = function() {
-          fileObj.value = reader.result;
-        };
-        reader.readAsDataURL(this.files[0]);
-      }
-
-
-      document.querySelector('#appstore-submit')
-        .addEventListener('click', (e) => {
-
-	  if (fileObj.value == "") {
-	    alert("Please attach a module zip file");
-	    return;
-	  }
-
-	  let newtx = app.wallet.createUnsignedTransactionWithDefaultFee(app.wallet.returnPublicKey(), 0.0);
-          if (newtx == null) {
-	    alert("Error sending transaction. Does your wallet have enough tokens?");
-	    return;
-	  }
-          newtx.transaction.msg.module   = "AppStore";
-  	  newtx.transaction.msg.request  = "submit module";
-  	  newtx.transaction.msg.module_module   = fileObj.value;
-  	  newtx = app.wallet.signTransaction(newtx);
-          app.network.propagateTransaction(newtx);
-
-	  alert("Module Submitted!");
-
-      });
-
-*/
     }
 
 }
