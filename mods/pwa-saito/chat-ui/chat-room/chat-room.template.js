722e583ba0b31f42d9654d87ff4130497e2d4ab9
BearGuy <stephen@peterkins.ca>
2019-11-29 19:31:39 +0700

    moved zip.js lib to saito/lib

--- mods/pwa-saito/chat-ui/chat-room/chat-room.template.js
+++
@@ -1,8 +0,0 @@
-export const ChatRoomTemplate = (room_id) => {
-    return `
-        <div id="${room_id}" class="chat-room">
-            <div id="${room_id}" class="chat-room-content">
-            </div>
-        </div>
-    `;
-}
