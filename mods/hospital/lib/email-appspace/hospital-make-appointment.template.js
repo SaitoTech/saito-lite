9efa102d339305df5a493df0eb8d903ff5fec9ac
David Lancashire <david@saito.tech>
2019-12-03 16:02:50 +0700

    alaunius push

--- mods/hospital/lib/email-appspace/hospital-make-appointment.template.js
+++ mods/hospital/lib/email-appspace/hospital-make-appointment.template.js
@@ -2,14 +2,28 @@ module.exports = HospitalMakeAppointmentTemplate = () => {
   return `
   <link rel="stylesheet" href="/hospital/css/email-appspace.css">
 
-  <h3>Available Appointments</h3>
-
-  <div class="calendar">
-    <input type="date" id="appointment-date" class="appointment-date" name="appointment-date" />
+  <div class="appointment-select-category">
+    <select class="appointment-select-category-select" name="appointment-select-category-select">>
+      <option value="selectl" selected>Select Appointment Category</option>
+      <option value="general">General Medicine</option>
+      <option value="pediatrics">Pediatrics</option>
+      <option value="dentistry">Dentistry</option>
+      <option value="cardiology">Cardiology</option>
+      <option value="Endocrinology">Endocrinology</option>
+    </select>
   </div>
 
-  <div class="appointments">
+
+  <div class="appointment-select-appointment">
+
+    Date Selected: <input type="date" id="appointment-date" class="appointment-date" name="appointment-date" />
+
+    <div class="appointments">
+    </div>
+
   </div>
+
+
 
     `;
 ***REMOVED***
