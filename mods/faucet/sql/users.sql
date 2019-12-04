e06f722aa6c9a0d3e2f4f826b5785b4683f684c9
Richard Parris <github@hda0.net>
2019-12-05 01:09:58 +0800

    new faucet

---
+++ mods/faucet/sql/users.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS users (
+    address TEXT,
+    tx_count INTEGER,
+    balance FLOAT,
+    games_finished INTEGER,
+    game_tx_count INTEGER,
+    first_tx INTEGER,
+    latest_tx INTEGER,
+    last_payout_ts INTEGER,
+    last_payout_amt FLOAT,
+    total_payout FLOAT,
+    total_spend FLOAT,
+    referer TEXT,
+    PRIMARY KEY (address asc)
+);
