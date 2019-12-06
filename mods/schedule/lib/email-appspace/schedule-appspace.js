a923557b2654a636878d0d4668f58314fcddc8e1
David Lancashire <david@saito.tech>
2019-12-06 10:27:21 +0700

    schedule module

--- mods/schedule/lib/email-appspace/schedule-appspace.js
+++ mods/schedule/lib/email-appspace/schedule-appspace.js
@@ -4,14 +4,36 @@ const ScheduleAppspaceTemplate 	= require('./schedule-appspace.template.js');
 module.exports = ScheduleAppspace = {
 
     render(app, data) {
-
       document.querySelector(".email-appspace").innerHTML = ScheduleAppspaceTemplate();
+      this.renderMonthCalendar();
+    },
 
+    attachEvents(app, data) {
+    },
+
+
+    renderDayCalendar() {
+      document.querySelector(".email-appspace").innerHTML = ScheduleAppspaceTemplate();
       var calendarEl = document.getElementById('schedule-calendar');
+      var calendar = new FullCalendar.Calendar(calendarEl, {
+        plugins: [ 'list' ],
+        defaultView: 'listWeek',
+        views: {
+          listDay: { buttonText: 'list day' },
+          listWeek: { buttonText: 'list week' },
+          listMonth: { buttonText: 'list month' },
+        },
+        events: [],
+	noEventsMessage: "No events to display",
+      });
+      calendar.render();
+    },
 
+    renderMonthCalendar() {
+      document.querySelector(".email-appspace").innerHTML = ScheduleAppspaceTemplate();
+      var calendarEl = document.getElementById('schedule-calendar');
       var calendar = new FullCalendar.Calendar(calendarEl, {
         plugins: [ 'dayGrid' ],
-/***
 	events: [
    	  {
       	    	title: 'Meeting',
@@ -27,17 +49,18 @@ module.exports = ScheduleAppspace = {
       		borderColor: 'green'
     	  },
         ],
-****/
-  	dayRender: function(dayRenderInfo) {
+  	dayRender:(dayRenderInfo) => {
           dayRenderInfo.el.innerHTML = '<div class="schedule-calendar-day"><div class="schedule-calendar-day-appointment-num"></div></div>';
+	  console.log("DAY INFO: " + dayRenderInfo.date);
+	  dayRenderInfo.el.onclick = () => {
+alert("Clicked!");
+            this.renderDayCalendar(dayRenderInfo.date);
+          }
   	},
       });
-
       calendar.render();
-
     },
 
-    attachEvents(app, data) {
-    }
+
 
 }
