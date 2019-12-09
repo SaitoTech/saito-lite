55444a9473613510376ef7fc86230cba6cfe1599
David Lancashire <david@saito.tech>
2019-12-09 13:45:56 +0700

    carousel migration part 1

--- mods/arcade/lib/arcade-main/arcade-loader.js
+++ mods/arcade/lib/arcade-main/arcade-loader.js
@@ -31,7 +31,7 @@ module.exports = ArcadeLoader = {
               app.options.games[i].ts = new Date().getTime();
               app.options.games[i].initialize_game_run = 0;
               app.storage.saveOptions();
-              window.location = '/' + app.options.games[i].module.toLowerCase().replace(' ', '-');
+              window.location = '/' + app.options.games[i].module.toLowerCase().replace(' ', '_');
               break;
         ***REMOVED***
       ***REMOVED***
