51888825897e07e248a10129896bfc5617450dc0
Richard Parris <github@hda0.net>
2019-12-10 00:47:03 +1100

    back on the carousel

--- mods/arcade/lib/arcade-main/arcade-game-carousel/arcade-game-carousel.template.js
+++ mods/arcade/lib/arcade-main/arcade-game-carousel/arcade-game-carousel.template.js
@@ -1,6 +1,8 @@
 module.exports = AracadeGameCarouselTemplate = () => {
   return `
   <link rel="stylesheet" href="/arcade/carousel.css" />
-  <div class="arcade-carousel-wrapper"></div>
+  <div class="arcade-carousel-wrapper">
+  <img src="./img/16by9.png" class="carousel-spacer" />
+  </div>
   `;
 ***REMOVED***
