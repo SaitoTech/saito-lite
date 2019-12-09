d027a2a80c9ae62367f3c01783228ea192154e14
David Lancashire <david@saito.tech>
2019-12-09 14:34:50 +0700

    carousel structure into lib files

--- mods/arcade/lib/arcade-main/arcade-game-carousel/arcade-game-carousel.template.js
+++ mods/arcade/lib/arcade-main/arcade-game-carousel/arcade-game-carousel.template.js
@@ -1,10 +1,6 @@
 module.exports = AracadeGameCarouselTemplate = () => {
   return `
-  <div class="arcade-main-hero">
-    <div class="arcade-hero">
-      <div class="arcade-hero-wrapper">
-      </div>
-    </div>
-  </div>
+  <link rel="stylesheet" href="/arcade/carousel.css" />
+  <div class="arcade-carousel-wrapper"></div>
   `;
 }
--- mods/arcade/lib/arcade-main/arcade-main.template.js
+++ mods/arcade/lib/arcade-main/arcade-main.template.js
@@ -1,6 +1,5 @@
 module.exports = ArcadeMainTemplate = () => {
   return `
-    <link rel="stylesheet" href="/arcade/hero.css" />
     <div class="arcade-game-carousel">
     </div>
     <ul class="arcade-gamelist" id="arcade-gamelist"></ul>
