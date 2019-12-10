70a7db7083d6664356c9b09fa3c841577d7e6584
BearGuy <stephen@peterkins.ca>
2019-12-10 14:05:37 +0700

    faucet tutorial modals

---
+++ mods/faucet/lib/modal/faucet-modal-captcha.template.js
@@ -0,0 +1,8 @@
+module.exports = FaucetModalIntroTemplate = () => {
+  return `
+    <div>
+      <p>Saito is an open-source blockchain network. We use tokens to do things on the network, lets get you some</p>
+      <div id="recaptcha"></div>
+    </div>
+  `
+***REMOVED***
