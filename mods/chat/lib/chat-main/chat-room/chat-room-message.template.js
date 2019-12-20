dc52d0d4d53a042110af9eb519db1b2f6191853e
Richard Parris <github@hda0.net>
2019-12-20 12:06:47 +0700

    #29 chat ui enhancements and cleanup

--- mods/chat/lib/chat-main/chat-room/chat-room-message.template.js
+++ mods/chat/lib/chat-main/chat-room/chat-room-message.template.js
@@ -2,13 +2,15 @@ const datetimeFormatter = require('../../../../../lib/helpers/datetime_formatter
 
 module.exports = ChatRoomMessageTemplate = ({ message, publickey, timestamp, identicon ***REMOVED***, sig, type) => {
   let datetime = datetimeFormatter(timestamp);
+  
   return `
     <div id="${sig***REMOVED***" class="chat-room-message chat-room-message-${type***REMOVED***">
       <img src="${identicon***REMOVED***" class="chat-room-message-identicon"/>
-      <p class="chat-message-text">${message***REMOVED***</p>
-      <div class="chat-message-header">
-          <p class="chat-message-author">${publickey***REMOVED***</p>
-          <p class="chat-message-timestamp">${datetime.hours***REMOVED***:${datetime.minutes***REMOVED***</p>
+      <div class="chat-message-text">${message***REMOVED***
+        <div class="chat-message-header">
+            <p class="chat-message-author">${publickey***REMOVED***</p>
+            <p class="chat-message-timestamp">${datetime.hours***REMOVED***:${datetime.minutes***REMOVED***</p>
+        </div>
       </div>
     </div>
   `
