7e2a867ffcca1c8aefb626e5c0cf55b99ec9c29a
Richard Parris <github@hda0.net>
2019-12-28 01:09:07 +0800

    removing creds

--- mods/mailrelay/lib/credentials.js
+++ mods/mailrelay/lib/credentials.js
@@ -1,18 +1,9 @@
 module.exports = {
-    host: "smtp.sendgrid.net",
+    host: "",
     port: 465,
     secure: true, // upgrade later with STARTTLS
     auth: {
-//      user: "YXBpa2V5",
-//      pass: "U0cuUDdKSHpYMGxSSVdHa2RuUlROSDJTQS5IZlNlVGpqR0pDbTNWbENOT0Y4R2lJRG52TDlvX1hBRWxpLUFWbXQ2a05V"
-//      user: "RichardParris",
-//      pass: "O@eFv^6OFfg3CnWZ98e1OE7frxwiSFt*W@",
-      user: "apikey",
-      pass: "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"
-    }/*,
-      tls: {
-         // do not fail on invalid certs
-         rejectUnauthorized: false,
-         ciphers:'SSLv3'
-      }*/
-};
+      user: "",
+      pass: ""
+    }
+}
