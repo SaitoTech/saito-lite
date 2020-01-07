0e453e04b22e574d01f80cf5cdf0dc3574162036
David Lancashire <david@saito.tech>
2020-01-07 11:27:19 +0800

    QRCode in dropdown menu

---
+++ mods/myqrcode/lib/email-appspace/myqrcode-appspace.template.js
@@ -0,0 +1,13 @@
+module.exports = MyQRCodeAppspaceTemplate = (app) => {
+  return `
+  <div class="email-appspace-settings">
+
+    <h3>Your QR Code:</h3>
+  
+    <p></p>
+
+    <div id="qrcode"></div>
+
+  </div>
+  `;
+}
