104ff025932c51763d5a24a7cf768497fb1a130f
zhangxiaoyu <zhangxiaoyu@zhangxioyudeMBP.lan>
2020-01-08 11:54:12 +0700

    profile css

--- mods/profile/lib/email-appspace/profile-appspace.template.js
+++ mods/profile/lib/email-appspace/profile-appspace.template.js
@@ -3,30 +3,30 @@ module.exports = ProfileAppspaceTemplate = (app) => {
   <link rel="stylesheet" href="/profile/style.css">
   <div class="email-appspace-profile">
 
-    <div class="">  
+    <div class="">
 
       <div class="grid-2">
         <div class="saito-address">Saito Address:</div>
         <div>unregistered</div>
-      </div> 
-          
+      </div>
+
       <div class="registering-saito-address-info">Registering a Saito address allows others to send you messages using your Saito username instead of having to copy and paste your publickey.</div>
-      
+
       <div class="grid-2">
         <div>Legacy Email:</div>
         <div>unregistered</div>
-      </div>  
-        
+      </div>
+
       <div class="registering-email-address-info">Registering an email address provides some initial tokens for using the network and provides an email address we can use to send you a backup copy of your wallet.</div>
-      
+
       <div class="grid-2 grid-2-special">
         <div>Avatar:</div>
         <div></div>
-      </div>  
+      </div>
 
         <div><img style="max-width:100px;max-height:100px" src="${app.keys.returnIdenticon(app.wallet.returnPublicKey())}" /></div>
         <div class="upload-avatar-info">Upload an Avatar and your friends will see this instead of your standard image. It may take a bit of time until your new friends see it.</div>
-  
+
     <div style="display:none"><input id="profile-update-avatar" class="profile-update-avatar" style="" type="file" /></div>
 
   </div>
