901d7c490f369e5f093def509859e68949a88778
Richard Parris <github@hda0.net>
2020-01-09 11:38:21 +0800

    email form polish

--- mods/email/lib/email-main/email-body/email-form/email-form.template.js
+++ mods/email/lib/email-main/email-body/email-form/email-form.template.js
@@ -2,24 +2,29 @@ module.exports = EmailFormTemplate = () => {
   return `
   <div class="email-compose">
   <div class="grid-2" style="grid-template-columns:6em 1fr">
-      <div>From:</div>
-      <div>
-          <input id="email-from-address" class="email-address" type="text" readonly>
-      </div>
-      <div>To:</div>
-      <div>
-          <input id="email-to-address" class="email-address" type="text" placeholder="Address">
-      </div>
-      <div class="hidden amount-label">Send Saito:</div>
-      <div class="hidden amount-value">
-          <input class="email-amount" type="number" placeholder="0.0">
-      </div>  
+    <div>From:</div>
+    <div>
+      <input id="email-from-address" class="email-address" type="text" readonly>
+    </div>
+    <div>To:</div>
+    <div>
+      <input id="email-to-address" class="email-address" type="text" placeholder="Address">
+    </div>
+    <div class="hidden amount-label">Send Saito:</div>
+    <div class="hidden amount-value">
+      <input class="email-amount" type="number" placeholder="0.0">
+    </div>  
   </div>
   
-  <div id="email-form-options">
+  <div id="email-form-options" class="tip">
     <!--i class="icon-med fas fa-paperclip"></i>
     <i class="icon-med fas fa-image"></i-->
-    <i class="icon-med fas fa-dollar-sign tip"><div class="tiptext left">Send Saito</div></i>
+    <i class="icon-small fas fa-dollar-sign"></i>
+    <div class="tiptext tip-left">
+      <div>
+        <i class="icon-small fas fa-dollar-sign"></i>Send Saito
+      </div>
+    </div>
   </div>
   <div>
       <input class="email-title" type="text" placeholder="Subject">
@@ -27,8 +32,7 @@ module.exports = EmailFormTemplate = () => {
   <div id="email-text" class="email-text markdown" placeholder="Message"></div>
   </div>
   <div>
-      <button class="email-submit">Send</button>
-      
+    <button class="email-submit">Send</button>
   </div>
   `
 };
