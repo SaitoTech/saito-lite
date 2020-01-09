b65c2c5af041891543f34ef2dcb85f98602ada93
David Lancashire <david@saito.tech>
2020-01-09 19:49:53 +0800

    profile update

--- mods/profile/lib/email-appspace/profile-appspace.template.js
+++ mods/profile/lib/email-appspace/profile-appspace.template.js
@@ -10,22 +10,22 @@ module.exports = ProfileAppspaceTemplate = (app) => {
         <div>unregistered</div>
       </div>
 
-      <div class="registering-saito-address-info">Registering a Saito address allows others to send you messages using your Saito username instead of having to copy and paste your publickey.</div>
+      <div class="profile-option-info registering-saito-address-info">Registering a Saito address allows others to send you messages using your Saito username instead of having to copy and paste your publickey.</div>
 
       <div class="grid-2">
         <div>Legacy Email:</div>
         <div>unregistered</div>
       </div>
 
-      <div class="registering-email-address-info">Registering an email address provides some initial tokens for using the network and provides an email address we can use to send you a backup copy of your wallet.</div>
+      <div class="profile-option-info registering-email-address-info">Registering an email address provides some initial tokens for using the network and provides an email address we can use to send you a backup copy of your wallet.</div>
 
       <div class="grid-2 grid-2-special">
-        <div>Avatar:</div>
+        <div>Your Avatar:</div>
         <div></div>
       </div>
 
-        <div><img style="max-width:100px;max-height:100px" src="${app.keys.returnIdenticon(app.wallet.returnPublicKey())}" /></div>
-        <div class="upload-avatar-info">Upload an Avatar and your friends will see this instead of your standard image. It may take a bit of time until your new friends see it.</div>
+      <div id="profile-avatar" class="profile-avatar"><img style="max-width:100px;max-height:100px" src="${app.keys.returnIdenticon(app.wallet.returnPublicKey())}" /></div>
+      <div class="profile-option-info upload-avatar-info">Upload an Avatar and your friends will see this instead of your standard image. It may take a bit of time until your new friends see it.</div>
 
   </div>
   `;
