404fb0300f09b9db100e8d8368fe750649467269
Richard Parris <github@hda0.net>
2020-01-12 23:45:08 +0800

    appstore popover basics

--- mods/appstore/lib/email-appspace/appstore-app-details/appstore-app-details.template.js
+++ mods/appstore/lib/email-appspace/appstore-app-details/appstore-app-details.template.js
@@ -3,39 +3,29 @@ module.exports = AppstoreAppDetailsTemplate = (app) => {
 
 <div class="appstore-app-install-overlay">
 
-  <div class="appstore-app-install-image">
-<img src="/saito/img/dreamscape.png" />
-  </div>
+  <div class="appstore-app-install-content">
 
-  <div class="appstore-app-install-name">
-Poker Leaderboards
-  </div>
+    <div class="appstore-app-install-image">
+      <img src="/saito/img/dreamscape.png" />
+    </div>
 
-  <div class="appstore-app-install-description">
-This is an exciting application that lets you track how you are doing playing poker compared to everyone else in the entire world. Can you make the top 1000 players?
-  </div>
+    <div class="appstore-app-install-name"></div>
 
-  <div class="appstore-app-install-version">
-This is an exciting application that lets you track how you are doing playing poker compared to everyone else in the entire world. Can you make the top 1000 players?
-  </div>
+    <div class="appstore-app-install-description"></div>
 
-  <div class="appstore-app-install-publickey">
-This is an exciting application that lets you track how you are doing playing poker compared to everyone else in the entire world. Can you make the top 1000 players?
-  </div>
+    <div class="appstore-app-install-version"></div>
 
-  <div class="appstore-app-install-unixtime">
-This is an exciting application that lets you track how you are doing playing poker compared to everyone else in the entire world. Can you make the top 1000 players?
-  </div>
+    <div class="appstore-app-install-publickey"></div>
 
-  <div class="appstore-app-install-bsh">
-This is an exciting application that lets you track how you are doing playing poker compared to everyone else in the entire world. Can you make the top 1000 players?
-  </div>
+    <div class="appstore-app-install-unixtime"></div>
 
-  <div class="appstore-app-install-bid">
-This is an exciting application that lets you track how you are doing playing poker compared to everyone else in the entire world. Can you make the top 1000 players?
-  </div>
+    <div class="appstore-app-install-bsh"></div>
 
-  <button id="appstore-app-install-confirm-btn" class="appstore-app-install-confirm-btn">INSTALL</button>
+    <div class="appstore-app-install-bid"></div>
+
+    <button id="appstore-app-install-confirm-btn" class="appstore-app-install-confirm-btn">INSTALL</button>
+  
+  </div>
 
 </div>
   `;
