439220042f423dd61011a0566aeca8012bab6b39
Richard Parris <github@hda0.net>
2020-01-14 15:12:26 +0800

    app store all but install stop completed

--- mods/appstore/lib/email-appspace/appstore-appspace-publish/appstore-publish.js
+++ mods/appstore/lib/email-appspace/appstore-appspace-publish/appstore-publish.js
@@ -82,7 +82,7 @@ module.exports = AppStorePublish = {
                 AppStorePublishSuccess.render();
                 AppStorePublishSuccess.attachEvents();
               } else {
-                alert("Please attach a zip file of your module");
+                salert("Please attach a zip file of your module");
               }
             }
   },
