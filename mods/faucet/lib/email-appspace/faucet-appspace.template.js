6b063f829bfbc29184357b1d60f5506b78f96c95
BearGuy <stephen@peterkins.ca>
2020-01-15 22:55:31 +0800

    fixed appstore by renaming rewards and twilight

--- mods/faucet/lib/email-appspace/faucet-appspace.template.js
+++
@@ -1,7 +0,0 @@
-module.exports = FaucetAppspaceTemplate = (app) => {
-  return `
-   <div class="email-appspace-faucet">
-    <h2>Saito TESTNET Rewards</h2>
-    <div class="faucet-grid"></div>
-  `;
-}
---
+++ mods/rewards/lib/email-appspace/rewards-appspace.template.js
@@ -0,0 +1,7 @@
+module.exports = RewardsAppspaceTemplate = (app) => {
+  return `
+    <div class="email-appspace-rewards">
+      <h2>Saito TESTNET Rewards</h2>
+    <div class="rewards-grid"></div>
+  `;
+}
