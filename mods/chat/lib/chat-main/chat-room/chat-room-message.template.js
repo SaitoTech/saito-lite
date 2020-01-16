6c4708e328802c198dfed60c532b2ea490dfabff
Richard Parris <github@hda0.net>
2020-01-17 03:18:33 +0800

    chat densification and then making it nice again.

--- mods/chat/lib/chat-main/chat-room/chat-room-message.template.js
+++ mods/chat/lib/chat-main/chat-room/chat-room-message.template.js
@@ -1,16 +1,9 @@
 module.exports = ChatRoomMessageTemplate = ({ message, publickey, timestamp, identicon }, sig, type, data) => {
-  let { datetime_formatter } = data.helpers;
-  let datetime = datetime_formatter(timestamp);
+  
 
   return `
     <div id="${sig}" class="chat-room-message chat-room-message-${type}">
-      <img src="${identicon}" class="chat-room-message-identicon"/>
-      <div class="chat-message-text">${message}
-        <div class="chat-message-header">
-            <p class="chat-message-author">${publickey.substring(0, 16)}</p>
-            <p class="chat-message-timestamp">${datetime.hours}:${datetime.minutes}</p>
-        </div>
-      </div>
+      <div class="chat-message-text">${message}</div>
     </div>
   `
 }
