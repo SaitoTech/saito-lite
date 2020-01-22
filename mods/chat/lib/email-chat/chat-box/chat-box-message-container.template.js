7b4f914476bfdb03459b3addade1d5b80b1e2b16
BearGuy <stephen@peterkins.ca>
2020-01-22 14:14:28 +0800

    chat cleanup, identicon color calculcations added to keychain, notification reversion to make compatible with chrome

--- mods/chat/lib/email-chat/chat-box/chat-box-message-container.template.js
+++ mods/chat/lib/email-chat/chat-box/chat-box-message-container.template.js
@@ -1,11 +1,17 @@
 const ChatRoomMessageTemplate = require('../../chat-main/chat-room/chat-room-message.template');
 
-module.exports = ChatBoxMessageContainerTemplate = ({ message, publickey, keyHTML, timestamp, identicon }, sig, type, data) => {
+module.exports = ChatBoxMessageContainerTemplate = ({
+  message,
+  keyHTML,
+  timestamp,
+  identicon,
+  identicon_color,
+  sig,
+  type
+}, data) => {
 
   let { datetime_formatter } = data.helpers;
   let datetime = datetime_formatter(timestamp);
-
-  let line_color = data.chat.app.crypto.base64ToString(identicon.split(",")[1]).split("fill:")[1].split(";")[0];
 
   return `
     <div class="chat-message-set chat-message-set-${type}">
@@ -15,8 +21,8 @@ module.exports = ChatBoxMessageContainerTemplate = ({ message, publickey, keyHTM
                 <p class="chat-message-author">${keyHTML}</p>
                 <p class="chat-message-timestamp">${datetime.hours}:${datetime.minutes}</p>
         </div>
-        <div class="chat-box-message-container chat-box-message-container-${type}" id="chat-box-message-container-${sig}" style="border-color:${line_color};">
-          ${ChatRoomMessageTemplate({ message, publickey, timestamp, identicon }, sig, type, data)}
+        <div class="chat-box-message-container chat-box-message-container-${type}" id="chat-box-message-container-${sig}" style="border-color:${identicon_color};">
+          ${ChatRoomMessageTemplate({ message, sig, type }, data)}
         </div>
       </div>
     </div>
