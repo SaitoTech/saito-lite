1ea5bf0c6a974bcbb0837b329515a5f97c0c36e6
BearGuy <stephen@peterkins.ca>
2020-02-17 20:54:01 +0700

    restructure email-chat to work with games

--- mods/chat/lib/email-chat/chat-list/chat-list.js
+++ mods/chat/lib/email-chat/chat-list/chat-list.js
@@ -1,53 +1,49 @@
 const ChatListTemplate = require('./chat-list.template');
 const ChatListRowTemplate = require('./chat-list-row.template');
-
-const ChatBox = require('../chat-box/chat-box');
 const ChatManager = require('../chat-manager/chat-manager');
 
-
 module.exports = ChatList = {
+
+    initialize(app, data) {
+        const render_chatlist_listener = () => {
+            this.render(app, data);
+            this.attachEvents(app, data);
+        };
+
+        app.connection.removeAllListeners("chat-render-request");
+        app.connection.on("chat-render-request", render_chatlist_listener);
+    },
 
     render(app, data) {
         let chat_list = document.querySelector('.chat-list');
         chat_list.innerHTML = "";
-        data.chat.groups.forEach(group => chat_list.innerHTML += ChatListRowTemplate(group, data.chat.helpers));
-        // if (data.chat.groups.length > 0 && data.chat.active_groups == 0) this.openChatBox(app, data, data.chat.groups[0]);
+        data.chat.groups.forEach(group => {
+            chat_list.innerHTML += ChatListRowTemplate(group, data.chat.helpers);
+        });
     },
 
     attachEvents(app, data) {
         Array.from(document.getElementsByClassName('chat-row'))
             .forEach(row => {
                 row.addEventListener('click', (e) => {
-
                     let group_id = e.currentTarget.id;
                     if (document.getElementById(`chat-box-${group_id}`)) {
-                      let textareaobj = document.getElementById(`chat-box-new-message-input-${group_id}`);
-                      textareaobj.focus();
-                      textareaobj.select();
+                      let chat_box_input = document.getElementById(`chat-box-new-message-input-${group_id}`);
+                      chat_box_input.focus();
+                      chat_box_input.select();
 
-		      //
-		      // maximize if minimized
-		      //
-                      let chatboxobj = document.getElementById(`chat-box-${group_id}`);
-                      chatboxobj.classList.remove("chat-box-hide");
+                      //
+                      // maximize if minimized
+                      //
+                      let chat_box = document.getElementById(`chat-box-${group_id}`);
+                      chat_box.classList.remove("chat-box-hide");
                       return;
                     }
 
                     let selected_group = data.chat.groups.filter(group => group.id == group_id);
-                    this.openChatBox(app, data, selected_group[0]);
+                    ChatManager.openChatBox(app, data, selected_group[0]);
                 });
             });
     },
-
-    openChatBox(app, data, selected_group,) {
-      ChatManager.addChatBox(app, data, selected_group);
-      data.chat.active_groups.push(selected_group);
-
-      // select textarea
-      let textareaobj = document.getElementById(`chat-box-new-message-input-${selected_group.id}`);
-      textareaobj.focus();
-      textareaobj.select();
-    }
-
 }
 
