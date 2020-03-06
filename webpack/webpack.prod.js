46344e10132fffcd1525e340bc2a8585890a0c46
David Lancashire <david@saito.tech>
2020-03-06 15:42:35 +0700

    debugging compile

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,7 +1,12 @@
 const path = require('path');
 const webpack = require('webpack');
 
+console.log("DR: " + __dirname);
+
 webpack({
+  optimization: {
+    minimize: false
+  },
   target: 'web',
     node: {
         fs: "empty",
@@ -24,29 +29,69 @@ webpack({
         },
         {
             webpack: 'webpack'
-        }
-    ],
+        },
+        /\.png$/,
+        /\.jpg$/,
+        /\.html$/,
+        /\.css$/,
+        /\.sql$/,
+        /\.md$/,
+        /\.pdf$/,
+        /\.sh$/,
+        /\.zip$/,
+        /\/web\//,
+        /\/www\//,
+        /\.txt/
+     ],
   entry: ["babel-polyfill", path.resolve(__dirname, './../bundler/default/lib/index.js')],
   output: {
-      path: path.resolve(__dirname, '../web/saito/'),
+      path: path.resolve(__dirname, './../web/saito/'),
       filename: 'saito.js'
   },
   module: {
     rules: [
-        {
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
+            {
+                test: /html$/,
+                exclude: [ /(mods)/, /(email)/ ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
-            }
-        },
+            },
+            // Emscripten JS files define a global. With `exports-loader` we can
+            // load these files correctly (provided the global’s name is the same
+            // as the file name).
+            {
+                test: /quirc\.js$/,
+                loader: "exports-loader"
+            },
+            // wasm files should not be processed but just be emitted and we want
+            // to have their public URL.
+            {
+                test: /quirc\.wasm$/,
+                type: "javascript/auto",
+                loader: "file-loader",
+                options: {
+                    publicPath: "dist/"
+                }
+            },
+            {
+                test: /\.zip$/,
+                exclude: [
+                    path.resolve(__dirname, '../mods/appstore/bundler'),
+                    path.resolve(__dirname, '../mods/appstore/mods'),
+                ]
+            },
     ]
   },
   mode: 'production',
-  devtool: false,
+  devtool: "eval",
   }, (err, stats) => {
   if (err || stats.hasErrors()) {
     console.log(err);
