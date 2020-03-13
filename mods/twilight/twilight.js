e12402310de7017c1c802c1cc6f2f6c865ede804
David Lancashire <david@saito.tech>
2020-03-13 22:00:21 +0700

    Brush War fixes

--- mods/twilight/twilight.js
+++ mods/twilight/twilight.js
@@ -9957,95 +9957,92 @@ console.log("\n\n\n\n");
 
           if (twilight_self.countries[i].control <= 2) {
 
-            let play_brush_war = 1;
             let divname = "#" + i;
 
             if (i === "italy" || i === "greece" || i === "spain" || i == "turkey") {
               if (twilight_self.game.state.events.nato == 1) {
                 if (twilight_self.isControlled("us", i) == 1) {
-                  play_brush_war = 0;
+                  twilight_self.displayModal("NATO prevents Brush War in Europe");
+                  return;
                 }
               }
             }
 
-            if (play_brush_war == 1) {
+            $(divname).off();
+            $(divname).on('click', function() {
 
-              $(divname).off();
-              $(divname).on('click', function() {
+              let c = $(this).attr('id');
 
-                let c = $(this).attr('id');
+              twilight_self.displayModal("Launching Brush War in "+twilight_self.countries[c].name);
 
-                twilight_self.displayModal("Launching Brush War in "+twilight_self.countries[c].name);
+              let dieroll = twilight_self.rollDice(6);
+              let modify = 0;
 
-                let dieroll = twilight_self.rollDice(6);
-                let modify = 0;
-
-                for (let v = 0; v < twilight_self.countries[c].neighbours.length; v++) {
-                  if (twilight_self.isControlled(opponent, v) == 1) {
-                    modify++;
-                  }
+              for (let v = 0; v < twilight_self.countries[c].neighbours.length; v++) {
+                if (twilight_self.isControlled(opponent, v) == 1) {
+                  modify++;
                 }
+              }
 
-                if (twilight_self.game.player == 1) {
-                  if (c == "mexico") { modify++; }
-                  if (c == "cuba") { modify++; }
-                  if (c == "japan") { modify++; }
-                  if (c == "canada") { modify++; }
-                }
-                if (twilight_self.game.player == 2) {
-                  if (c == "finland") { modify++; }
-                  if (c == "romania") { modify++; }
-                  if (c == "afghanistan") { modify++; }
-                  if (c == "northkorea") { modify++; }
-                }
+              if (twilight_self.game.player == 1) {
+                if (c == "mexico") { modify++; }
+                if (c == "cuba") { modify++; }
+                if (c == "japan") { modify++; }
+                if (c == "canada") { modify++; }
+              }
+              if (twilight_self.game.player == 2) {
+                if (c == "finland") { modify++; }
+                if (c == "romania") { modify++; }
+                if (c == "afghanistan") { modify++; }
+                if (c == "northkorea") { modify++; }
+              }
                 
-                dieroll = dieroll - modify;
+              dieroll = dieroll - modify;
 
-                if (dieroll >= 3) {
+              if (dieroll >= 3) {
 
-                  let usinf = twilight_self.countries[c].us;
-                  let ussrinf = twilight_self.countries[c].ussr;
+                let usinf = twilight_self.countries[c].us;
+                let ussrinf = twilight_self.countries[c].ussr;
 
-                  if (me == "us") {
-                    twilight_self.removeInfluence(c, ussrinf, "ussr");
-                    twilight_self.placeInfluence(c, ussrinf, "us");
-                    twilight_self.addMove("remove\tus\tussr\t"+c+"\t"+ussrinf);
-                    twilight_self.addMove("place\tus\tus\t"+c+"\t"+ussrinf);
-                    twilight_self.addMove("milops\tus\t3");
-                    if (twilight_self.game.state.events.flowerpower == 1) {
-                      twilight_self.addMove("vp\tus\t1\t1");
-                    } else {
-                      twilight_self.addMove("vp\tus\t1");
-                    }
-                     twilight_self.endTurn();
+                if (me == "us") {
+                  twilight_self.removeInfluence(c, ussrinf, "ussr");
+                  twilight_self.placeInfluence(c, ussrinf, "us");
+                  twilight_self.addMove("remove\tus\tussr\t"+c+"\t"+ussrinf);
+                  twilight_self.addMove("place\tus\tus\t"+c+"\t"+ussrinf);
+                  twilight_self.addMove("milops\tus\t3");
+                  if (twilight_self.game.state.events.flowerpower == 1) {
+                    twilight_self.addMove("vp\tus\t1\t1");
                   } else {
-                    twilight_self.removeInfluence(c, usinf, "us");
-                    twilight_self.placeInfluence(c, usinf, "ussr");
-                    twilight_self.addMove("remove\tussr\tus\t"+c+"\t"+usinf);
-                    twilight_self.addMove("place\tussr\tussr\t"+c+"\t"+usinf);
-                    twilight_self.addMove("milops\tussr\t3");
-                    if (twilight_self.game.state.events.flowerpower == 1) {
-                      twilight_self.addMove("vp\tussr\t1\t1");
-                    } else {
-                      twilight_self.addMove("vp\tussr\t1");
-                    }
+                    twilight_self.addMove("vp\tus\t1");
                   }
-                  twilight_self.addMove("notify\tBrush War in "+twilight_self.countries[c].name+" succeeded.");
-                  twilight_self.addMove("notify\tBrush War rolls "+dieroll +" (bonus: -"+modifier);
                   twilight_self.endTurn();
-
                 } else {
-                  if (me == "us") {
-                    twilight_self.addMove("milops\tus\t3");
+                  twilight_self.removeInfluence(c, usinf, "us");
+                  twilight_self.placeInfluence(c, usinf, "ussr");
+                  twilight_self.addMove("remove\tussr\tus\t"+c+"\t"+usinf);
+                  twilight_self.addMove("place\tussr\tussr\t"+c+"\t"+usinf);
+                  twilight_self.addMove("milops\tussr\t3");
+                  if (twilight_self.game.state.events.flowerpower == 1) {
+                    twilight_self.addMove("vp\tussr\t1\t1");
                   } else {
-                    twilight_self.addMove("milops\tussr\t3");
+                    twilight_self.addMove("vp\tussr\t1");
                   }
-                  twilight_self.addMove("notify\tBrush War in "+twilight_self.countries[c].name+" failed.");
-                  twilight_self.addMove("notify\tBrush War rolls "+dieroll +" (bonus: -"+modifier);
-                  twilight_self.endTurn();
                 }
-              });
-            }
+                twilight_self.addMove("notify\tBrush War in "+twilight_self.countries[c].name+" succeeded.");
+                twilight_self.addMove("notify\tBrush War rolls "+dieroll +" (bonus: -"+modifier);
+                twilight_self.endTurn();
+
+              } else {
+                if (me == "us") {
+                  twilight_self.addMove("milops\tus\t3");
+                } else {
+                  twilight_self.addMove("milops\tussr\t3");
+                }
+                twilight_self.addMove("notify\tBrush War in "+twilight_self.countries[c].name+" failed.");
+                twilight_self.addMove("notify\tBrush War rolls "+dieroll +" (bonus: -"+modifier);
+                twilight_self.endTurn();
+              }
+            });
           }
         }
       }
