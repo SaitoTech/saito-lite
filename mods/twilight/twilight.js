62e1ff628267ff906d3778e4bc439dc0c1476e03
David Lancashire <david@saito.tech>
2020-03-15 00:02:59 +0700

    truman fix

--- mods/twilight/twilight.js
+++ mods/twilight/twilight.js
@@ -6990,17 +6990,12 @@ console.log("\n\n\n\n");
 
       var twilight_self = this;
 
-      twilight_self.playerFinishedPlacingInfluence();
-      twilight_self.addMove("resolve\ttruman");
-
       var options_purge = [];
-
       for (var i in this.countries) {
         if (i == "canada" || i == "uk" || i == "france" || i == "spain" || i == "greece" || i == "turkey" || i == "italy" || i == "westgermany" || i == "eastgermany" || i == "poland" || i == "benelux" || i == "denmark" || i == "norway" || i == "finland" || i == "sweden" || i == "yugoslavia" || i == "czechoslovakia" || i == "bulgaria" || i == "hungary" || i == "romania" || i == "austria") {
           if (twilight_self.countries[i].ussr > 0 && twilight_self.isControlled('ussr', i) != 1 && twilight_self.isControlled('us', i) != 1) { options_purge.push(i); }
         }
       }
-
 
       if (options_purge.length == 0) {
         this.updateLog("USSR has no influence that can be removed by Truman");
@@ -7008,43 +7003,44 @@ console.log("\n\n\n\n");
       }
 
       if (options_purge.length == 1) {     
-
         twilight_self.removeInfluence(options_purge[0], twilight_self.countries[options_purge[0]].ussr, "ussr");
         this.updateLog("Truman removes all USSR influence from " + options_purge[0]);
         return 1;
+      }
 
-      } else {
 
-        if (this.game.player == 1) {
-          this.updateStatus("US is selecting target for Truman");
-          return 0;
-        }
+      if (this.game.player == 1) {
+        this.updateStatus("US is selecting target for Truman");
+        return 0;
+      }
 
-        if (this.game.player == 2) {
+      if (this.game.player == 2) {
 
-          twilight_self.updateStatus("Select a non-controlled country in Europe to remove all USSR influence: ");
+        twilight_self.updateStatus("Select a non-controlled country in Europe to remove all USSR influence: ");
 
-          for (let i = 0; i < options_purge.length; i++) {
+        twilight_self.playerFinishedPlacingInfluence();
+        twilight_self.addMove("resolve\ttruman");
 
-            let countryname  = options_purge[i];
-            let divname      = '#'+countryname;
+        for (let i = 0; i < options_purge.length; i++) {
 
-            twilight_self.countries[countryname].place = 1;
+          let countryname  = options_purge[i];
+          let divname      = '#'+countryname;
 
-            $(divname).off();
-            $(divname).on('click', function() {
+          twilight_self.countries[countryname].place = 1;
 
-              let c = $(this).attr('id');
-              let ussrpur = twilight_self.countries[c].ussr;
+          $(divname).off();
+          $(divname).on('click', function() {
 
-              twilight_self.removeInfluence(c, ussrpur, "ussr", function() {
-                twilight_self.addMove("notify\tUS removes all USSR influence from "+twilight_self.countries[c].name);
-                twilight_self.addMove("remove\tus\tussr\t"+c+"\t"+ussrpur);
-                twilight_self.playerFinishedPlacingInfluence();
-                twilight_self.endTurn();
-              });
+            let c = $(this).attr('id');
+            let ussrpur = twilight_self.countries[c].ussr;
+
+            twilight_self.removeInfluence(c, ussrpur, "ussr", function() {
+              twilight_self.addMove("notify\tUS removes all USSR influence from "+twilight_self.countries[c].name);
+              twilight_self.addMove("remove\tus\tussr\t"+c+"\t"+ussrpur);
+              twilight_self.playerFinishedPlacingInfluence();
+              twilight_self.endTurn();
             });
-          }
+          });
         }
       }
 
