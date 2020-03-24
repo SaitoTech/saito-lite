727dbbe30c01bab398c9535794a3487f683472e4
Richard Parris <github@hda0.net>
2020-03-24 23:54:49 +0800

    qr sacanner updates - landscape

--- mods/tutorial05/lib/chat-navbar/transfer-manager.template.js
+++ mods/tutorial05/lib/chat-navbar/transfer-manager.template.js
@@ -4,8 +4,8 @@ module.exports = TransferManagerTemplate = (mode) => {
     return `
       <div class="transfer-manager-container">
         
-        <div class="scanner-placeholder" style="border: 3px solid var(--saito-red); margin: 10vw; height: 80vw;"></div>
-        <button class="super launch-scanner" style="margin: 0 10vw; width: 80vw;"><i class="fas fa-qrcode"></i> Scan</button>
+        <div class="scanner-placeholder"></div>
+        <button class="super launch-scanner"><i class="fas fa-qrcode"></i> Scan</button>
       </div>
     `;
   } else {
