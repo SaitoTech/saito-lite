6cac223a2132813bfd5455326c8d7f7816153299
David Lancashire <david@saito.tech>
2020-03-27 23:10:36 +0700

    push table fix

--- mods/covid19/lib/supplier-portal.template.js
+++ mods/covid19/lib/supplier-portal.template.js
@@ -17,18 +17,17 @@ module.exports = SupplierPortalTemplate = () => {
 
     <div class="portal" style="display:none">
 
-      <table id="products-table" class="products-table" style="display:none;">
-        <tr>
-          <th>Category</th>
-          <th>Specification</th>
-          <th>Photo</th>
-          <th>Unit Cost</th>
-          <th>Daily Volume</th>
-          <th>Certifications</th>
-          <th>Lead Time</th>
-          <th></th>
-        </tr>
-      </table>
+      <div id="products-table" class="products-table" style="display:none;grid-gap: 1em;grid-template-columns: repeat(8, auto);">
+          <div class="table-head">Category</div>
+          <div class="table-head">Specification</div>
+          <div class="table-head">Photo</div>
+          <div class="table-head">Unit Cost</div>
+          <div class="table-head">Daily Volume</div>
+          <div class="table-head">Certifications</div>
+          <div class="table-head">Lead Time</div>
+          <div class="table-head"></div>
+      </div>
+
 
     </div>
   </div>
