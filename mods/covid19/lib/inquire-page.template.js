ba630786e686490695e9766214038a825506975f
Richard Parris <github@hda0.net>
2020-04-07 23:18:09 +0800

    adding inqire page

--- mods/covid19/lib/inquire-page.template.js
+++ mods/covid19/lib/inquire-page.template.js
@@ -4,7 +4,7 @@ module.exports = ProductPageTemplate = () => {
 
   html = `
 
-  <h3>Product Enqirey</h3>
+  <h3>Product Inquiry</h3>
 
   <p>
 
@@ -14,14 +14,11 @@ module.exports = ProductPageTemplate = () => {
 
   <div class="inq-grid grid-2-columns" id="inq-grid"></div>
 
-
-
   <h4>Contact Methods</h4>
 
   <div class="contact-grid grid-2-columns">
-<div>Email:</div><div>dangerD@theDouggPound.com</div>
-<div>Phone:</div><div>+123 4567 8910</div>
-
+    <div><i class="far fa-envelope"></i> Email:</div><div>dangerD@theDouggPound.com</div>
+    <div><i class="fas fa-phone-alt"></i> Phone:</div><div>+123 4567 8910</div>
   </div>
 
   <button id="keep-shopping">Go back to list</button>
