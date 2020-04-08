1d44ec0c5d690c5bd55472b67fb54b904c7c2a88
Richard Parris <github@hda0.net>
2020-04-08 18:36:03 +0800

    fix to supplier grid column ordering

--- mods/covid19/lib/supplier-portal.js
+++ mods/covid19/lib/supplier-portal.js
@@ -16,9 +16,20 @@ module.exports = SupplierPortal = {
       //
       let whereclause = `suppliers.id = products.supplier_id AND products.category_id = categories.id AND suppliers.publickey = "${app.wallet.returnPublicKey()}"`;
       data.covid19.sendPeerDatabaseRequest("covid19", "products JOIN suppliers LEFT JOIN categories", "products.id as 'product_id', categories.name, products.product_specification, products.product_photo, products.pricing_per_unit_rmb, products.production_daily_capacity, products.id", whereclause, null, function(res) {
+        var html = `
+          <div class="table-head">Product</div>
+          <div class="table-head">Photo</div>
+          <div class="table-head">Unit Cost</div>
+          <div class="table-head">Certifications</div>
+          <div class="table-head">Daily Volume</div>
+          <div class="table-head"></div>
+          <div class="table-head"></div>
+        `;
+        document.querySelector(".products-table").innerHTML = html;
+
 
         // no 'name' on supplier page
-        data.covid19.addProductsToTable(res.rows, [ 'name', 'product_specification', 'product_photo', 'production_daily_capacity', 'certifications', 'id', 'admin'], app, data);
+        data.covid19.addProductsToTable(res.rows, [ 'product_specification', 'product_photo', 'pricing_per_unit_rmb', 'certifications', 'production_daily_capacity', 'id', 'admin'], app, data);
 
       document.querySelector(".loading").style.display = "none";
       document.querySelector(".portal").style.display = "block";
