ea247de0ade9e8480400a2adac1caf07918ee086
David Lancashire <david@saito.tech>
2020-04-29 18:12:25 +0700

    removing button

--- mods/covid19/lib/product-page.template.js
+++ mods/covid19/lib/product-page.template.js
@@ -41,7 +41,9 @@ module.exports = ProductPageTemplate = (app, data) => {
 
       <div style="display:flex;" class="product-buttons">
       
+<!--
         <div id="attachments-${data.product_id}" class="attachments-btn button"><i class="fas fa-list"></i> View Attachments</div>
+-->
       </div>
 
     </div>
