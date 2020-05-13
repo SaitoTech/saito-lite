6cc01ee998126d2bf890d74a9990ba008fb46217
Richard Parris <github@hda0.net>
2020-05-14 02:36:08 +0800

    interim covid push

--- mods/covid19/lib/splash-page-admin.template.js
+++ mods/covid19/lib/splash-page-admin.template.js
@@ -8,6 +8,7 @@ module.exports = SplashPageAdminTemplate = () => {
     <h2>Admin Portal</h2>
 
     <p><a class="" href="/covid19/?mode=category-manager">Categories and Pricing</a></p>
+    <p><a class="" href="/covid19/?mode=order-manager">Orders</a></p>
     <p><a class="" href="/covid19/?mode=file-manager">File Manager</a></p>
     <p><a class="" href="/covid19/?mode=bundle-manager">Bundle Mananger</a></p>
     <p><a class="" href="/covid19/?mode=category-manager">Categories and Pricing</a></p>
