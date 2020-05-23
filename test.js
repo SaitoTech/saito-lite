e1acaf74cf181d80776feb8695294715af263b10
David Lancashire <david@saito.tech>
2020-05-23 14:52:26 +0700

    utf16le

---
+++ test.js
@@ -0,0 +1,7 @@
+
+  let x = "oh fuck me, it also is affecting my FF browser, since i send the email to my FF's wallet.:thumbsup:";
+  
+  let z = Buffer.from(x, 'utf16le').toString('utf8');
+
+console.log(z);
+
