c70ca91532f4db44e256c12a878b64d7ab8273bf
escapedcat <github@htmlcss.de>
2020-05-24 13:35:05 +0800

    chore: integrate webpack-dev-server
    it is running but might have broken things somewhere else

--- README.md
+++ README.md
@@ -1,18 +1,35 @@
 # Welcome to Saito
 
-Please see the documents in the /docs directory for instructions on how to 
-install Saito, learn how the network works, or just get started building 
+Please see the documents in the [/docs directory](docs) for [instructions on how to
+install Saito](docs/INSTALL.md), learn how the network works, or just get started building
 applications on the network.
 
-If you need to get in touch with us, please reach out anytime. 
+If you need to get in touch with us, please reach out anytime.
 
 The Saito Team
 info@saito.tech
 
-## build (node version >= 12)
+## build adn run full-node (server) (node version >= 12)
 
-```
-npm install
-npm run nuke
-npm start
-```
+1. install deps
+   ```
+   npm install
+   ```
+1. compile saito
+   ```
+   npm run nuke
+   ```
+   Might take a while. Wait till `Bundle Success!`
+1. run local dev
+   ```
+   npm start
+   ```
+   http://localhost:12101/
+
+## dev workflow for light-node (client)
+
+1. run client
+   ```
+   npm run client
+   ```
+   http://localhost:8888/
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,120 +1,64 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
 
-
-webpack({
-  optimization: {
-    minimize: false
-  },
-  target: 'web',
+module.exports = {
+  mode: "development",
   node: {
     fs: "empty",
   },
   externals: [
-        {
-            archiver: 'archiver'
-        },
-        {
-            child_process: 'child_process'
-        },
-        {
-            nodemailer: 'nodemailer'
-        },
-        {
-            jimp: 'jimp'
-        },
-        {
-            "image-resolve": "image-resolver"
-        },
-        {
-            sqlite: 'sqlite'
-        },
-        {
-            unzipper: 'unzipper'
-        },
-        {
-            webpack: 'webpack'
-        },
-        // /^(image-resolver|\$)$/i,
-        /\.txt/,
-        /\.png$/,
-        /\.jpg$/,
-        /\.html$/,
-        /\.css$/,
-        /\.sql$/,
-        /\.md$/,
-        /\.pdf$/,
-        /\.sh$/,
-        /\.zip$/,
-        /\/web\//,
-        /\/www\//
+    {
+      archiver: "archiver",
+    },
+    {
+      child_process: "child_process",
+    },
+    {
+      nodemailer: "nodemailer",
+    },
+    {
+      jimp: "jimp",
+    },
+    {
+      "image-resolve": "image-resolver",
+    },
+    {
+      sqlite: "sqlite",
+    },
+    {
+      unzipper: "unzipper",
+    },
+    {
+      webpack: "webpack",
+    },
+    // /^(image-resolver|\$)$/i,
+    /\.txt/,
+    /\.png$/,
+    /\.jpg$/,
+    /\.html$/,
+    /\.css$/,
+    /\.sql$/,
+    /\.md$/,
+    /\.pdf$/,
+    /\.sh$/,
+    /\.zip$/,
+    /\/web\//,
+    /\/www\//,
   ],
-  // Path to your entry point. From this file Webpack will begin his work
-  entry: ["babel-polyfill", path.resolve(__dirname, './../bundler/default/lib/saito/lite/index.js')],
+  entry: [
+    "babel-polyfill",
+    // path.resolve(__dirname, "./../bundler/default/lib/saito/lite/index.js"),
+    path.resolve(__dirname, "./../lib/saito/lite/index.js"),
+  ],
   output: {
-    path: path.resolve(__dirname, './../web/saito'),
-    filename: 'saito.js'
+    path: path.resolve(__dirname, "./../web/saito"),
+    filename: "saito.bundle.js",
   },
-  module: {
-        rules: [
-            {
-                test: /html$/,
-                exclude: [ /(mods)/, /(email)/ ],
-            },
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            // Emscripten JS files define a global. With `exports-loader` we can
-            // load these files correctly (provided the global’s name is the same
-            // as the file name).
-            {
-                test: /quirc\.js$/,
-                loader: "exports-loader"
-            },
-            // wasm files should not be processed but just be emitted and we want
-            // to have their public URL.
-            {
-                test: /quirc\.wasm$/,
-                type: "javascript/auto",
-                loader: "file-loader",
-                options: {
-                    publicPath: "dist/"
-                }
-            },
-	    {
-                test: /\.zip$/,
-                exclude: [
-                    path.resolve(__dirname, '../mods/appstore/bundler'),
-                    path.resolve(__dirname, '../mods/appstore/mods'),
-                ]
-            },
-        ]
+  devServer: {
+    contentBase: path.resolve(__dirname, "./../web"),
+    port: 8888,
+    host: "0.0.0.0",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
   },
-
-  mode: 'production',
-  devtool: "cheap-module-eval-source-map",
-  //devtool: false,
-  //devtool: "eval",
-
-  }, (err, stats) => {
-  if (err || stats.hasErrors()) {
-    console.log(err);
-    if (stats) {
-      let info = stats.toJson();
-      console.log(info.errors);
-    }
-  }
-  //
-  // Done processing
-  //
-  console.log("Bundle Success!");
-});
-
-
+};
