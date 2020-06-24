0608d26c3a38d50326e2e60859467cd4ab2b388e
David Lancashire <david@saito.tech>
2020-06-24 15:34:05 +0700

    photograph module

--- mods/photograph/photograph.js
+++ mods/photograph/photograph.js
@@ -47,47 +47,47 @@ class Photograph extends ModTemplate {
         document.getElementById("qr-canvas")
       );
 
-      alert("You have clicked me!");
-
     });
 
   }
 
-
-/***
-        var imgData = this.canvas_context.getImageData(0, 0, this.canvas.width, this.canvas.height);
-
-        if (imgData.data) {
-          this.decoder.postMessage(imgData);
-        }
-        return 1;
-****/
 
 
   async start(video, canvas) {
 
     this.video = video
     this.canvas = canvas;
-
     this.canvas_context = this.canvas.getContext("2d");
 
     try {
       let stream = await navigator.mediaDevices.getUserMedia(this.constraints);
       this.handleSuccess(stream);
     } catch (err) {
-console.log("ERROR!: " + err);
     }
 
     this.startCameraLoop();
   }
 
 
+
   startCameraLoop() {
 
+    //
+    // we try to write an image from the camera to the canvas
+    // if it succeeds, then we attach the events to the buttons
+    // that must be showing on the camera screen
+    //
     x = this.attemptVideoCapture();
-
     if (x == 1) {
       console.log("working...");
+
+      //
+      // attach event to button
+      //
+      document.querySelector(".capture-the-moment-btn").addEventListener('click', (e) => {
+	this.takePhotograph();
+      });
+
     } else {
       console.log("wait 100....");
       setTimeout(() => {
@@ -97,6 +97,9 @@ console.log("ERROR!: " + err);
 
   }
 
+
+
+  
 
   //
   // main loop sending messages to quirc_worker to detect qrcodes on the page
@@ -108,29 +111,36 @@ console.log("ERROR!: " + err);
         this.canvas.width = this.video.videoWidth;
         this.canvas.height = this.video.videoHeight;
         this.canvas_context.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
-        if (this.canvas.width == 0) { return; }
 
-        var imgData = this.canvas_context.getImageData(0, 0, this.canvas.width, this.canvas.height);
-
-        if (imgData.data) {
-console.log("DATA CAPTURED!");
-//          this.decoder.postMessage(imgData);
-        }
-
-        return 1;
-
+        if (this.canvas.width == 0) { return 0; }
       } catch (err) {
-        return 0;
-console.log("ERROR CAPTURING DATA");
-        //if (err.name == 'NS_ERROR_NOT_AVAILABLE') { setTimeout(() => { this.attemptQRDecode() }, 0); return; }
-        //setTimeout(() => { this.attemptQRDecode() }, 500);
+	console.log("ERROR: " + err);
+	return 0;
       }
-
-    } else {
-console.log("isStreamInit not initialized");
-      return 0;
     }
-    return 0;
+
+    return 1;
+  }
+
+  takePhotograph() {
+
+    //
+    // take another (refreshed) image
+    //
+    this.attemptVideoCapture();
+    this.isStreamInit = false;
+
+    //
+    // grab image
+    //
+    var image = new Image();
+    image.id = "pic";
+    image.src = this.canvas.toDataURL();
+    document.body.innerHTML = "";
+    document.body.appendChild(image);
+
+    return 1;
+
   }
 
 
@@ -157,9 +167,19 @@ console.log("isStreamInit not initialized");
           <video playsinline autoplay id="qr-video" style="height: 100vh;width: 100vw;"></video>
         </div>
         <canvas style="display: none" id="qr-canvas"></canvas>
-	<div id="capture-the-moment-btn" class="capture-the-moment-btn" style="position:absolute; width:80%;height:140px; background-color:white">CAPTURE THE MOMENT</div>
+	<div id="capture-the-moment-btn" class="capture-the-moment-btn" style="top:40px;left:20px;position:absolute;width:80%;height:140px;z-index:9999;background-color:white">CAPTURE THE MOMENT</div>
       </div>
 
+    `;
+
+  }
+
+
+  returnPhotoHTML(imgdata) {
+
+    return `
+      <div class="">
+      </div>
     `;
 
   }
