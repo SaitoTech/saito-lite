b56f18dea86736e9d2bae99f07077d85adf3c1eb
David Lancashire <david@saito.tech>
2020-07-06 23:05:35 +0700

    Webpack update to shrink

--- mods/imperium/src/actions/action.js
+++ mods/imperium/src/actions/action.js
@@ -1,3 +1,23 @@
+/************************************
+  
+ACTION CARD - types
+
+"action" -> main menu
+"bombardment_attacker"
+"bombardment_defender"
+"combat"
+"ground_combat"
+"pds" -> before pds fire
+"post_pds" -> after pds fire
+"pre_agenda" --> before agenda voting
+"post_agenda" --> after agenda voting
+"space_combat"
+"space_combat_victory"
+"rider"
+
+
+************************************/
+
 
 
     this.importActionCard('lost-star-chart', {
--- mods/imperium2/src/imperium-end.js
+++
@@ -1,9 +0,0 @@
-
-
-
-
-}
-
-module.exports = Imperium;
-  
-
