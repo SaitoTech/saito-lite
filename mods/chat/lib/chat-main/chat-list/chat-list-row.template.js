2de83bf5e9e89fd3d7c453f17fde4bb8bba5fb07
Richard Parris <github@hda0.net>
2020-07-22 22:33:44 +0800

    fix for sidebar chat display for images and html

--- mods/chat/lib/chat-main/chat-list/chat-list-row.template.js
+++ mods/chat/lib/chat-main/chat-list/chat-list-row.template.js
@@ -3,12 +3,16 @@ module.exports = ChatListRowTemplate = ({name, group_id, message, timestamp, is_
   let datetime = datetime_formatter(timestamp);
   let lock_icon_html = is_encrypted ? '<div style="justify-self: center;"><i class="fas fa-lock" style="color: black;"></i></div>' : '';
 
+  let tmp = document.createElement("DIV");
+  tmp.innerHTML = this.app.crypto.base64ToString(message);
+  let clean_message = tmp.innerText;
+
   return `
     <div id="chat-row-${group_id}" class="chat-row">
       <img src="/saito/img/logo-color.svg">
       <div class="chat-content">
           <div class="chat-group-name">${name}</div>
-          <div class="chat-last-message">${message.substring(0, 72)}</div>
+          <div class="chat-last-message">${clean_message.substring(0, 72)}</div>
       </div>
       <div style="display: grid;">
         <div class="chat-last-message-timestamp">${datetime.hours}:${datetime.minutes}</div>
