c4cfe0434d6c6c1de4f8c9d5c76bf80446e9290c
Richard Parris <github@hda0.net>
2020-07-30 15:38:51 +0800

    adding sector info

--- mods/imperium/src/imperium-initialize.js
+++ mods/imperium/src/imperium-initialize.js
@@ -328,6 +328,7 @@
         ' \
           <div class="hexIn" id="hexIn_'+i+'"> \
             <div class="hexLink" id="hexLink_'+i+'"> \
+            <div class="hexInfo" id="hexninfo_'+i+'"></div> \
               <div class="hex_bg" id="hex_bg_'+i+'"> \
                 <img class="hex_img sector_graphics_background '+this.game.board[i].tile+'" id="hex_img_'+i+'" src="" /> \
                 <img src="/imperium/img/frame/border_full_white.png" id="hex_img_faction_border_'+i+'" class="faction_border" /> \
@@ -347,9 +348,12 @@
       // insert planet
       let planet_div = "#hex_img_"+i;
       $(planet_div).attr("src", this.game.sectors[this.game.board[i].tile].img);
+
+      // add planet info
   
       this.updateSectorGraphics(i);
-  
+
+        
           
     }
   
