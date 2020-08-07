90ef77a239857f3bffbecd8347711a8cc0c5bcd0
David Lancashire <david@saito.tech>
2020-08-07 23:08:02 +0700

    fixing imperium

--- mods/imperium/src/factions/sol.js
+++ mods/imperium/src/factions/sol.js
@@ -2,8 +2,8 @@
     this.importFaction('faction1', {
       name		: 	"Federation of Sol",
       homeworld		: 	"sector52",
-//      space_units	:	["carrier","carrier","destroyer","fighter","fighter","fighter"],
       space_units	:	["carrier","carrier","destroyer","fighter","fighter","fighter"],
+//      space_units	:	["carrier","carrier","destroyer","fighter","fighter","fighter"],
       ground_units	:	["infantry","infantry","infantry","infantry","infantry","spacedock"],
       tech		:	["neural-motivator","antimass-deflectors", "faction1-orbital-drop", "faction1-versatile", "faction1-flagship"],
       background	: 	"faction1.jpg",
