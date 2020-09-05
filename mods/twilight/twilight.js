72b5d0b492da98f95065e48ac4785fa6f99461ca
David Lancashire <david@saito.tech>
2020-09-06 00:23:15 +0700

    Terrorism

--- mods/twilight/twilight.js
+++ mods/twilight/twilight.js
@@ -11339,56 +11339,50 @@ console.log("card: " + card);
       if (this.game.player == 2 && target == "us") {
 
         let available_cards = this.game.deck[0].hand.length;
+        let cards_for_select = [];
         for (let z = 0; z < this.game.deck[0].hand.length; z++) {
-          if (this.game.deck[0].hand[i] === "china" || this.game.deck[0].hand[i] == this.game.state.headline_opponent_card || this.game.deck[0].hand[i] == this.game.state.headline_card) { cards_to_reveal--; }
+          if (this.game.deck[0].hand[i] === "china" || this.game.deck[0].hand[i] == this.game.state.headline_opponent_card || this.game.deck[0].hand[i] == this.game.state.headline_card) { cards_to_reveal--; } else {
+	    cards_for_select.push(this.game.deck[0].hand[z]);
+	  }
         }
-        if (available_cards < cards_to_discard) { cards_to_discard = available_cards; }
-
+        if (cards_for_select.length < cards_to_discard) { cards_to_discard = cards_for_select.length; }
         if (cards_to_discard == 0) { this.addMove("notify\tUS has no cards to discard"); }
 
 
         for (let i = 0; i < cards_to_discard; i++) {
-          this.rollDice(twilight_self.game.deck[0].hand.length, function(roll) {
-            roll = parseInt(roll)-1;
-            let card = twilight_self.game.deck[0].hand[roll];
-
-            if (card == "china") {
-              if (roll-2 >= 0) { card = twilight_self.game.deck[0].hand[roll-2]; } else {
-                card = twilight_self.game.deck[0].hand[roll];
-              }
-            }
-
-            twilight_self.removeCardFromHand(card);
-            twilight_self.addMove("dice\tburn\tussr");
-            twilight_self.addMove("discard\tus\t"+card);
+	  if (cards_for_select.length > 0) {
+            this.rollDice(cards_for_select.length, function(roll) {
+              roll = parseInt(roll)-1;
+              let card = cards_for_select[roll];
+              twilight_self.removeCardFromHand(card);
+              twilight_self.addMove("dice\tburn\tussr");
+              twilight_self.addMove("discard\tus\t"+card);
               twilight_self.addMove("notify\t"+target.toUpperCase()+" discarded "+twilight_self.game.deck[0].cards[card].name);
-          });
+	      cards_for_select.splice(roll, 1);
+            });
+	  }
         }
         twilight_self.endTurn();
       }
       if (this.game.player == 1 && target == "ussr") {
 
+        let cards_for_select = [];
         let available_cards = this.game.deck[0].hand.length;
         for (let z = 0; z < this.game.deck[0].hand.length; z++) {
-          if (this.game.deck[0].hand[z] == "china") { available_cards--; }
+          if (this.game.deck[0].hand[z] == "china") { available_cards--; } else {
+	    cards_for_select.push(this.game.deck[0].hand[z]);
+	  }
         }
-        if (available_cards < cards_to_discard) { cards_to_discard = available_cards; }
+        if (cards_for_select.length < cards_to_discard) { cards_to_discard = cards_for_select.length; }
 
         if (cards_to_discard == 0) { this.addMove("notify\tUSSR has no cards to discard"); this.endTurn(); return 0; }
-        this.rollDice(twilight_self.game.deck[0].hand.length, function(roll) {
+        this.rollDice(cards_for_select.length, function(roll) {
             roll = parseInt(roll)-1;
-            let card = twilight_self.game.deck[0].hand[roll];
-
-            if (card == "china") {
-              if (roll-2 >= 0) { card = twilight_self.game.deck[0].hand[roll-2]; } else {
-                card = twilight_self.game.deck[0].hand[roll];
-              }
-            }
-
+            let card = cards_for_select[roll];
             twilight_self.removeCardFromHand(card);
             twilight_self.addMove("dice\tburn\tus");
             twilight_self.addMove("discard\tussr\t"+card);
-              twilight_self.addMove("notify\t"+target.toUpperCase()+" discarded "+twilight_self.game.deck[0].cards[card].name);
+            twilight_self.addMove("notify\t"+target.toUpperCase()+" discarded "+twilight_self.game.deck[0].cards[card].name);
             twilight_self.endTurn();
         });
       }
