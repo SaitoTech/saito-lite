314da9b5e79ade7fabda28be5ced0e8c4f3fb4f4
Richard Parris <github@hda0.net>
2020-11-02 00:38:33 +0800

    frontpage updates

--- mods/website/website.js
+++ mods/website/website.js
@@ -7,25 +7,25 @@ class Website extends ModTemplate {
   constructor(app) {
     super(app);
 
-    this.app            = app;
-    this.name           = "Website";
-    
+    this.app = app;
+    this.name = "Website";
+
     this.description = "Investor information frontend";
-    this.categories  = "Utilities Communications";
+    this.categories = "Utilities Communications";
 
     return this;
   }
 
 
-/*
-  initialize(app){
-
-    x = this.app.modules.respondTo("email-chat");
-    for (let i = 0; i < x.length; i++) {
-      this.mods.push(x[i]);
+  /*
+    initialize(app){
+  
+      x = this.app.modules.respondTo("email-chat");
+      for (let i = 0; i < x.length; i++) {
+        this.mods.push(x[i]);
+      }
     }
-  }
-*/
+  */
 
   initializeHTML(app) {
     console.log("Website initializeHTML")
@@ -48,9 +48,50 @@ class Website extends ModTemplate {
     `;
     var iconlist = document.querySelector('.header-icon-links');
     iconlist.insertBefore(app.browser.htmlToElement(html), iconlist.firstChild);
-    
+
+    this.updatePage(app);
+
   }
 
+  async onConfirmation(blk, tx, conf, app) {
+    if (conf == 0) {
+      this.updatePage(app);
+    }
+  }
+
+  updatePage(app) {
+    if (document.querySelector('.frontpage-balance')) {
+      try {
+        //let balance = this.app.wallet.returnBalance();
+        let balance = this.app.wallet.returnBalance();
+        document.querySelector('.frontpage-balance').innerHTML = balance + " SAITO";
+      } catch (err) { }
+    }
+
+    if (document.querySelector('.frontpage-address')) {
+      let publickey = app.wallet.returnPublicKey();
+      let id = app.keys.returnIdentifierByPublicKey(publickey, true);
+      document.querySelector('.frontpage-address').innerHTML = id;
+    }
+
+    if (document.querySelector('.register_username')) {
+      let id = "";
+      let publickey = app.wallet.returnPublicKey();
+      if (app.keys.returnIdentifierByPublicKey(publickey)) {
+        document.querySelector('.register_username').innerHTML = "Welcome Back";
+      } else {
+        document.querySelector('.register_username')
+          .onclick = async () => {
+            var requested_id = await sprompt("Pick a handle or nickname. <br /><sub>Alphanumeric characters only - Do not include an @</sub.>");
+            let register_success = app.modules.returnModule('Registry').registerIdentifier(requested_id);
+            if (register_success) {
+              id = `"${requested_id}@saito" requested.`;
+              document.querySelector('.register_username').innerHTML = id;
+            }
+          };
+      }
+    }
+  }
 
   shouldAffixCallbackToModule() { return 1; }
 
