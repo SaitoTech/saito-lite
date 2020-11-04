eb0696ecd61938369c4da80c354caddffa20ac97
Richard Parris <github@hda0.net>
2020-11-04 17:17:56 +0800

    updating structure to match scaffold

--- mods/arcade/lib/arcade-main/arcade-main.template.js
+++ mods/arcade/lib/arcade-main/arcade-main.template.js
@@ -1,5 +1,11 @@
 module.exports = ArcadeMainTemplate = () => {
   return `
-    <div class="arcade-main"></div>
+    <div class="arcade-middle">
+      <div class="arcade-main"></div>
+      <div class="arcade-sub">
+        <div class="arcade-latest"></div>
+        <div class="arcade-chatman"></div>
+      </div>
+    </div>
   `;
 }
