20e8dd7401b68cb16c2e20ca47a51aa40fbf16d0
David Lancashire <david@saito.tech>
2020-11-06 14:49:40 +0400

    posts scaffold

--- mods/arcade/lib/arcade-main/templates/arcade-posts.template.js
+++ mods/arcade/lib/arcade-main/templates/arcade-posts.template.js
@@ -1,6 +1,127 @@
 module.exports = ArcadePostsTemplate = () => {
   return `
     <div id="arcade-posts" class="arcade-posts">
+
+      <div data-id="" id="arcade-post-" class="arcade-post">
+        <div id="arcade-post-title" class="arcade-post-title">Saito selected for Web3 Foundation gaming standard</div>
+        <div id="arcade-post-sublinks" class="arcade-post-sublinks">
+	  <div id="" class="arcade-post-posted-by">posted by </div>
+	  <div id="" class="arcade-post-user">david@saito</div>
+	  <div id="" class="arcade-post-comments">2 comments</div>
+	  <div id="" class="arcade-post-report">report</div>
+        </div>
+      </div> 
+
+      <div data-id="" id="arcade-post-" class="arcade-post">
+        <div id="arcade-post-title" class="arcade-post-title">Saito selected for Web3 Foundation gaming standard</div>
+        <div id="arcade-post-sublinks" class="arcade-post-sublinks">
+	  <div id="" class="arcade-post-posted-by">posted by </div>
+	  <div id="" class="arcade-post-user">david@saito</div>
+	  <div id="" class="arcade-post-comments">2 comments</div>
+	  <div id="" class="arcade-post-report">report</div>
+        </div>
+      </div> 
+
+      <div data-id="" id="arcade-post-" class="arcade-post">
+        <div id="arcade-post-title" class="arcade-post-title">Saito selected for Web3 Foundation gaming standard</div>
+        <div id="arcade-post-sublinks" class="arcade-post-sublinks">
+	  <div id="" class="arcade-post-posted-by">posted by </div>
+	  <div id="" class="arcade-post-user">david@saito</div>
+	  <div id="" class="arcade-post-comments">2 comments</div>
+	  <div id="" class="arcade-post-report">report</div>
+        </div>
+      </div> 
+
+      <div data-id="" id="arcade-post-" class="arcade-post">
+        <div id="arcade-post-title" class="arcade-post-title">Saito selected for Web3 Foundation gaming standard</div>
+        <div id="arcade-post-sublinks" class="arcade-post-sublinks">
+	  <div id="" class="arcade-post-posted-by">posted by </div>
+	  <div id="" class="arcade-post-user">david@saito</div>
+	  <div id="" class="arcade-post-comments">2 comments</div>
+	  <div id="" class="arcade-post-report">report</div>
+        </div>
+      </div> 
+
+      <div data-id="" id="arcade-post-" class="arcade-post">
+        <div id="arcade-post-title" class="arcade-post-title">Saito selected for Web3 Foundation gaming standard</div>
+        <div id="arcade-post-sublinks" class="arcade-post-sublinks">
+	  <div id="" class="arcade-post-posted-by">posted by </div>
+	  <div id="" class="arcade-post-user">david@saito</div>
+	  <div id="" class="arcade-post-comments">2 comments</div>
+	  <div id="" class="arcade-post-report">report</div>
+        </div>
+      </div> 
+
+      <div data-id="" id="arcade-post-" class="arcade-post">
+        <div id="arcade-post-title" class="arcade-post-title">Saito selected for Web3 Foundation gaming standard</div>
+        <div id="arcade-post-sublinks" class="arcade-post-sublinks">
+	  <div id="" class="arcade-post-posted-by">posted by </div>
+	  <div id="" class="arcade-post-user">david@saito</div>
+	  <div id="" class="arcade-post-comments">2 comments</div>
+	  <div id="" class="arcade-post-report">report</div>
+        </div>
+      </div> 
+
+      <div data-id="" id="arcade-post-" class="arcade-post">
+        <div id="arcade-post-title" class="arcade-post-title">Saito selected for Web3 Foundation gaming standard</div>
+        <div id="arcade-post-sublinks" class="arcade-post-sublinks">
+	  <div id="" class="arcade-post-posted-by">posted by </div>
+	  <div id="" class="arcade-post-user">david@saito</div>
+	  <div id="" class="arcade-post-comments">2 comments</div>
+	  <div id="" class="arcade-post-report">report</div>
+        </div>
+      </div> 
+
     </div>
+
+<style type="text/css">
+
+.arcade-sub {
+  grid-template-columns: 6fr 3fr;
+}
+
+
+.arcade-posts {
+  font-size: 1.3em;
+}
+.arcade-post {
+  margin-bottom: 40px;
+}
+
+.arcade-post-sublinks {
+  clear: both;
+  font-size:0.8em;
+}
+.arcade-post-title {
+  font-weight: bold;  
+}
+
+.arcade-post-user {
+  float: left;
+  font-weight: bold;  
+  margin-left: 1em;
+}
+.arcade-post-title {
+  float: left;
+  font-weight: bold;  
+}
+.arcade-post-comments {
+  float: left;
+  font-weight: bold;  
+  margin-left: 1em;
+  text-decoration: underline;
+}
+.arcade-post-report {
+  float: left;
+  font-weight: bold;  
+  margin-left: 1em;
+  text-decoration: underline;
+}
+.arcade-post-posted-by {
+  float: left;
+}
+
+</style>
+
   `;
 }
