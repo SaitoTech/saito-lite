e9af0d649f312035682a8af1b622eaa2dde235bd
clay <claytonrabenda@gmail.com>
2020-11-10 12:39:23 +0800

    demo of invite tile style

--- mods/arcade/lib/arcade-main/templates/arcade-main.template.js
+++ mods/arcade/lib/arcade-main/templates/arcade-main.template.js
@@ -4,7 +4,15 @@ module.exports = ArcadeMainTemplate = () => {
     
       <div id="arcade-hero" class="arcade-hero">
 
-      <div class="a2 arcade-tile">asdf</div>
+      <div class="a2 arcade-tile twilight-struggle-invite">
+        <div class="invite-tile-left">Twilight Struggle</div>
+        <div class="invite-tile-right">
+          <div>Team:</div><div>US</div>
+          <div>Deck:</div><div>Twilight Absurdum</div>
+          <div>US Bonus:</div><div>10</div>
+          <div>Time Limit:</div><div>120 Minutes</div>
+        </div>
+      </div>
       <div class="a2 arcade-tile"></div>
       <div class="a2 arcade-tile"></div>
       <div class="a2 arcade-tile"></div>
