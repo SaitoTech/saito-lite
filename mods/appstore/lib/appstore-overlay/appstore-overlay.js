fe381d715b13cbf5353a7e90a6ce8fbe4211fb18
David Lancashire <david@saito.tech>
2020-11-10 19:36:08 +0400

    appstore working in email

--- mods/appstore/lib/appstore-overlay/appstore-overlay.js
+++ mods/appstore/lib/appstore-overlay/appstore-overlay.js
@@ -1,6 +1,7 @@
 const AppStoreOverlayTemplate = require('./appstore-overlay.template.js');
+const AppStoreOverlayAppTemplate = require('./appstore-overlay-app.template.js');
 const SaitoOverlay = require('./../../../../lib/saito/ui/saito-overlay/saito-overlay');
-
+const AppStoreAppDetails = require('./appstore-app-details/appstore-app-details.js');
 
 module.exports = AppStoreAppspace = {
 
@@ -12,10 +13,77 @@ module.exports = AppStoreAppspace = {
 
     mod.overlay.showOverlay(app, mod, AppStoreOverlayTemplate());
 
+    //
+    // fetch modules from appstore
+    //
+    mod.sendPeerDatabaseRequestWithFilter(
+
+        "AppStore" ,
+
+        `SELECT name, description, version, publickey, unixtime, bid, bsh FROM modules WHERE featured = 1` ,
+
+        (res) => {
+
+        if (res.rows != undefined) {
+          let installed_apps = [];
+          if (app.options.modules) {
+            for (let i = 0; i < app.options.modules.length; i++) {
+              installed_apps.push(app.options.modules[i].name);
+            }
+          }
+          for (let z = 0; z < res.rows.length; z++) {
+            if (installed_apps.includes(res.rows[z].name) || res.rows[z].name == "name" || res.rows[z].name == "Unknown") {
+              res.rows.splice(z, 1);
+              z--;
+            } else {
+            }
+          }
+          try {
+
+	    //
+	    // show content
+	    //
+            document.querySelector(".appstore-overlay-grid").innerHTML = "";
+            for (let i = 0; i < res.rows.length; i++) {
+              app.browser.addElementToDom(AppStoreOverlayAppTemplate(app, res.rows[i]), "appstore-overlay-grid");
+	    }
+
+    	    //
+    	    // install module (button)
+      	    //
+    	    Array.from(document.getElementsByClassName("appstore-overlay-app")).forEach(installbtn => {
+      	      installbtn.onclick = (e) => {
+                let module_obj = JSON.parse(app.crypto.base64ToString(e.currentTarget.id));
+		let data = {};
+                data.module = module_obj;
+                AppStoreAppDetails.render(app, data);
+                AppStoreAppDetails.attachEvents(app, data);
+	      };
+    	    });
+
+          } catch (err) {
+          }
+        }
+      }
+    );
   },
 
 
+
   attachEvents(app, mod) {
+
+    //
+    // install module (button)
+    //
+    Array.from(document.getElementsByClassName("appstore-app-install-btn")).forEach(installbtn => {
+
+      installbtn.onclick = (e) => {
+        let module_obj = JSON.parse(app.crypto.base64ToString(e.currentTarget.id));
+        data.module = module_obj;
+        AppStoreAppDetails.render(app, data);
+        AppStoreAppDetails.attachEvents(app, data);
+      };
+    });
 
   }
 
--- mods/appstore/lib/appstore-overlay/appstore-overlay.template.js
+++ mods/appstore/lib/appstore-overlay/appstore-overlay.template.js
@@ -1,4 +1,4 @@
-module.exports = AppStoreAppspaceTemplate = () => {
+module.exports = AppStoreAppspaceTemplate = (app, row) => {
 
   return `
   
@@ -6,120 +6,122 @@ module.exports = AppStoreAppspaceTemplate = () => {
 
   <div class="appstore-header-featured grid-2">
     <div>Install Applications:</div>
-    <div class="searchbox">
-        <input type="text" placeholder="search for apps..." id="appstore-search-box">
+    <div class="searchbox appstore-overlay-searchbox">
+        <input type="text" class="appstore-search-box" placeholder="search for apps..." id="appstore-search-box">
     </div>
   </div>
 
-  <div class="appstore-overlay-grid">
-
-    <div class="appstore-overlay-app">
-      <div class="appstore-overlay-app-image" style="background-image: url('/twilight/img/arcade.jpg')"></div>
-      <div class="appstore-overlay-app-details">
-        <div class="appstore-overlay-app-title">Twilight Struggle</div>
-        <div class="appstore-overlay-app-type">Financial, Strategy</div>
-      </div>
-    </div>
-
-    <div class="appstore-overlay-app">
-      <div class="appstore-overlay-app-image" style="background-image: url('/twilight/img/arcade.jpg')"></div>
-      <div class="appstore-overlay-app-details">
-        <div class="appstore-overlay-app-title">Twilight Struggle</div>
-        <div class="appstore-overlay-app-type">Financial, Strategy</div>
-      </div>
-    </div>
-
-    <div class="appstore-overlay-app">
-      <div class="appstore-overlay-app-image" style="background-image: url('/twilight/img/arcade.jpg')"></div>
-      <div class="appstore-overlay-app-details">
-        <div class="appstore-overlay-app-title">Twilight Struggle</div>
-        <div class="appstore-overlay-app-type">Financial, Strategy</div>
-      </div>
-    </div>
-
-    <div class="appstore-overlay-app">
-      <div class="appstore-overlay-app-image" style="background-image: url('/twilight/img/arcade.jpg')"></div>
-      <div class="appstore-overlay-app-details">
-        <div class="appstore-overlay-app-title">Twilight Struggle</div>
-        <div class="appstore-overlay-app-type">Financial, Strategy</div>
-      </div>
-    </div>
-
-    <div class="appstore-overlay-app">
-      <div class="appstore-overlay-app-image" style="background-image: url('/twilight/img/arcade.jpg')"></div>
-      <div class="appstore-overlay-app-details">
-        <div class="appstore-overlay-app-title">Twilight Struggle</div>
-        <div class="appstore-overlay-app-type">Financial, Strategy</div>
-      </div>
-    </div>
-
-    <div class="appstore-overlay-app">
-      <div class="appstore-overlay-app-image" style="background-image: url('/twilight/img/arcade.jpg')"></div>
-      <div class="appstore-overlay-app-details">
-        <div class="appstore-overlay-app-title">Twilight Struggle</div>
-        <div class="appstore-overlay-app-type">Financial, Strategy</div>
-      </div>
-    </div>
-
-    <div class="appstore-overlay-app">
-      <div class="appstore-overlay-app-image" style="background-image: url('/twilight/img/arcade.jpg')"></div>
-      <div class="appstore-overlay-app-details">
-        <div class="appstore-overlay-app-title">Twilight Struggle</div>
-        <div class="appstore-overlay-app-type">Financial, Strategy</div>
-      </div>
-    </div>
-
-    <div class="appstore-overlay-app">
-      <div class="appstore-overlay-app-image" style="background-image: url('/twilight/img/arcade.jpg')"></div>
-      <div class="appstore-overlay-app-details">
-        <div class="appstore-overlay-app-title">Twilight Struggle</div>
-        <div class="appstore-overlay-app-type">Financial, Strategy</div>
-      </div>
-    </div>
-
-    <div class="appstore-overlay-app">
-      <div class="appstore-overlay-app-image" style="background-image: url('/twilight/img/arcade.jpg')"></div>
-      <div class="appstore-overlay-app-details">
-        <div class="appstore-overlay-app-title">Twilight Struggle</div>
-        <div class="appstore-overlay-app-type">Financial, Strategy</div>
-      </div>
-    </div>
-
-    <div class="appstore-overlay-app">
-      <div class="appstore-overlay-app-image" style="background-image: url('/twilight/img/arcade.jpg')"></div>
-      <div class="appstore-overlay-app-details">
-        <div class="appstore-overlay-app-title">Twilight Struggle</div>
-        <div class="appstore-overlay-app-type">Financial, Strategy</div>
-      </div>
-    </div>
-
-    <div class="appstore-overlay-app">
-      <div class="appstore-overlay-app-image" style="background-image: url('/twilight/img/arcade.jpg')"></div>
-      <div class="appstore-overlay-app-details">
-        <div class="appstore-overlay-app-title">Twilight Struggle</div>
-        <div class="appstore-overlay-app-type">Financial, Strategy</div>
-      </div>
-    </div>
-
-    <div class="appstore-overlay-app">
-      <div class="appstore-overlay-app-image" style="background-image: url('/twilight/img/arcade.jpg')"></div>
-      <div class="appstore-overlay-app-details">
-        <div class="appstore-overlay-app-title">Twilight Struggle</div>
-        <div class="appstore-overlay-app-type">Financial, Strategy</div>
-      </div>
-    </div>
-
+  <div class="appstore-overlay-grid" id="appstore-overlay-grid">
   </div>
-
 
   <fieldset class="appstore-overlay-developers">
     <p>
-      Developer? Code your first Saito Application in five minutes. Click here to learn how.
+      Developer? Code and upload your first Saito Application in five minutes.
     </p>
   </fieldset>
 
 </div>
 <style type="text/css">
+
+.appstore-bundler-install-notice {
+  font-size: 1.2em;
+  height: 300px;
+  width: 600px;
+  display: block;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  border: 1px solid var(--saito-red);
+  background-color: var(--saito-cyber-black);
+  color: #fff;
+  font-family: visuelt-light;
+  padding: 1em;
+  transform: translate(-50%, -50%);
+}
+
+
+
+.appstore-app-install-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: #0009;
+  width: 100%;
+  height: 100%;
+  font-size: 1.1em;
+  z-index: 10000;
+}
+
+.appstore-app-install-content {
+  width: 80%;
+  max-width: 800px;
+  min-width: 400px;
+  padding: 1em;
+  color: var(--saito-white);
+  font-family: visuelt-light;
+  line-height: 1.5em;
+  background-color: var(--saito-cyber-black);
+  margin: auto;
+  position: relative;
+  top: 40%;
+  transform: translateY(-50%);
+  z-index: 5;
+  display: grid;
+  grid-gap: 1em;
+  grid-template-columns: 1fr 3fr;
+}
+
+.appstore-app-install-version {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 100%;
+  white-space: nowrap;
+}
+
+.appstore-app-install-image img {
+  width: 250px;
+}
+
+.appstore-app-istall-details.grid-2 {
+  grid-gap: 0.5em;
+  margin: 0;
+}
+
+.appstore-app-istall-details.grid-2 div{
+  padding: 0;
+  margin: 0;
+}
+
+.appstore-app-install-name {
+  position: absolute;
+  z-index: 5;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-shadow: 0 0 2px #000;
+}
+
+.appstore-app-install-image {
+  position: relative;
+}
+
+.appstore-app-install-description {
+  height: 3em;
+  overflow-y: auto;
+  margin: 0 0 1em 0;
+}
+
+
+
+.appstore-search-box {
+  width: 300px;
+  float: right;
+  font-size:0.8em;
+}
+
+.appstore-overlay-developers {
+  font-size:0.5em;
+}
 
 .appstore-overlay-container {
   background-color: whitesmoke;
@@ -132,6 +134,7 @@ module.exports = AppStoreAppspaceTemplate = () => {
   display: grid;
   grid-gap: 1em;
   grid-template-columns: 1fr 1fr 1fr;
+  margin-bottom:20px;
 }
 
 .appstore-overlay-app {
@@ -143,7 +146,14 @@ module.exports = AppStoreAppspaceTemplate = () => {
   font-size:20px;
   padding: 10px;
   min-height: 100px;
+  background-color: whitesmoke;
+  color: #444;
 }
+.appstore-overlay-app:hover {
+  background-color: var(--saito-red);
+  color: whitesmoke;
+}
+
 
 .appstore-overlay-app-image {
   background-size: cover;
@@ -161,6 +171,35 @@ module.exports = AppStoreAppspaceTemplate = () => {
 .appstore-overlay-app-type {
   font-size: 0.9em;
 }
+
+.appstore-overlay-app-btn {
+  padding-top: 4px;
+  padding-bottom: 4px;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 14px;
+  visibility: hidden;
+}
+
+.appstore-overlay-app-btn:hover {
+  visibility: visible;
+}
+
+.appstore-overlay-app-author {
+  margin-top: 10px;
+}
+
+.appstore-overlay-searchbox {
+  max-width: 300px;
+  float: right;
+  font-size: 0.5em;
+}
+
+.appstore-header-featured {
+  grid-template-columns: 1fr auto;
+}
+
 
 </style>
 
