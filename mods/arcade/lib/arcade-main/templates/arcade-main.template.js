c825c8993ba9a34f4a763ca3685c37a8ec469243
clay <claytonrabenda@gmail.com>
2020-11-10 21:56:51 +0800

    arcade style work

--- mods/arcade/lib/arcade-main/templates/arcade-main.template.js
+++ mods/arcade/lib/arcade-main/templates/arcade-main.template.js
@@ -3,22 +3,27 @@ module.exports = ArcadeMainTemplate = () => {
     <div id="arcade-main" class="arcade-main">
     
       <div id="arcade-hero" class="arcade-hero">
-
-      <div class="a2 arcade-tile twilight-struggle-invite">
-        <div class="invite-tile-left">Twilight Struggle</div>
-        <div class="invite-tile-right">
-          <div>Team:</div><div>US</div>
-          <div>Deck:</div><div>Twilight Absurdum</div>
-          <div>US Bonus:</div><div>10</div>
-          <div>Time Limit:</div><div>120 Minutes</div>
+      
+        <div class="a2 arcade-tile twilight-struggle-invite">
+          <div class="invite-tile-cancel-button">X</div>
+          <div class="invite-tile-join-button">JOIN</div>
+          <div class="invite-tile-top">
+            <span class="invite-tile-title">Twilight Struggle</span>
+          </div>
+          <div class="invite-tile-bottom">
+            <div>Team:</div><div>US</div>
+            <div>Deck:</div><div>Twilight Absurdum</div>
+            <div>US Bonus:</div><div>10</div>
+            <div>Time Limit:</div><div>120 Minutes</div>
+          </div>
         </div>
-      </div>
-      <div class="a2 arcade-tile"></div>
-      <div class="a2 arcade-tile"></div>
-      <div class="a2 arcade-tile"></div>
-      <div class="a2 arcade-tile"></div>
-      <div class="a2 arcade-tile"></div>
-      <div class="a2 arcade-tile"></div>
+        
+        <div class="a2 arcade-tile"></div>
+        <div class="a2 arcade-tile"></div>
+        <div class="a2 arcade-tile"></div>
+        <div class="a2 arcade-tile"></div>
+        <div class="a2 arcade-tile"></div>
+        <div class="a2 arcade-tile"></div>
 
       </div>
       <div id="arcade-sub" class="arcade-sub">
