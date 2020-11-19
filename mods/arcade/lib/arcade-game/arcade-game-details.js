4584b1cb0caaeaf7f3c4fc1aee588145713b3f1f
David Lancashire <david@saito.tech>
2020-11-19 13:54:35 +0400

    game creation update

--- mods/arcade/lib/arcade-game/arcade-game-details.js
+++ mods/arcade/lib/arcade-game/arcade-game-details.js
@@ -21,23 +21,45 @@ module.exports = ArcadeGameDetails = {
 
   render(app, mod, invite) {
 
+    let gamemod = app.modules.returnModule(invite.msg.game);
+    //let gamemod = app.modules.returnModule("Twilight");
+
     if (!document.getElementById("background-shim")) {
       app.browser.addElementToDom(`<div id="background-shim" class="background-shim" style=""><div id="background-shim-cover" class="background-shim-cover"></div></div>`); 
     }
+
+console.log("INVITE: " + JSON.stringify(invite));
 
     mod.overlay.showOverlay(app, mod, ArcadeGameDetailsTemplate(app, mod, invite));
     mod.meta_overlay = new AdvancedOverlay(app, mod);
     mod.meta_overlay.render(app, mod);
     mod.meta_overlay.attachEvents(app, mod);
 
-    let gamemod = app.modules.returnModule("Twilight");
     let gamemod_url = "/" + gamemod.returnSlug() + "/img/arcade.jpg";
     document.querySelector('.game-image').src = gamemod_url;
     document.querySelector('.background-shim').style.backgroundImage = 'url(' + gamemod_url + ')';
 
     document.querySelector('.game-wizard-options-toggle').onclick = (e) => {
+
+      if (!document.querySelector('.game-wizard-advanced-return-btn')) {
+        mod.meta_overlay.showOverlay(app, mod, gamemod.returnGameOptionsHTML());
+      }
       document.querySelector('.game-wizard-advanced-options-overlay').style.display = "block";
-      mod.meta_overlay.showOverlay(app, mod, gamemod.returnGameOptionsHTML());
+
+      //
+      // move into advanced menu
+      //
+      try {
+        if (document.getElementById("game-wizard-advanced-return-btn")) {
+          document.querySelector('.game-wizard-advanced-return-btn').onclick = (e) => {
+alert("hide overlay");
+            //mod.meta_overlay.hideOverlay();
+  	    document.getElementById("game-wizard-advanced-options-overlay").style.display = "none";
+          }
+        }
+      } catch (err) {
+console.log("ERR: " + err);
+      }
     };
 
     if (gamemod.publisher_message) {
@@ -81,26 +103,25 @@ try {
 
       let options = getOptions();
 
+console.log("OPTIONS: " + JSON.stringify(options));
+
+      let gamemod = app.modules.returnModule(options.gamename);
+
       let gamedata = {
         name: gamemod.name,
         slug: gamemod.returnSlug(),
         options: gamemod.returnFormattedGameOptions(options),
         options_html: gamemod.returnGameRowOptionsHTML(options),
-        players_needed: document.querySelector('.game-players-select').value,
+        players_needed: document.querySelector('.game-wizard-players-select').value,
       };
 
-      let players_needed = document.querySelector('.game-players-select').value;
+      let players_needed = document.querySelector('.game-wizard-players-select').value;
 
       if (players_needed == 1) {
         data.arcade.launchSinglePlayerGame(app, data, gamedata);
         return;
       } else {
-        document.querySelector('.game-details').toggleClass('hidden');
-        document.querySelector('.game-start-controls').toggleClass('hidden');
-        document.querySelector('.game-invite-controls').toggleClass('hidden');
-        if (players_needed >= 3) {
-          document.querySelector('#link-invite').toggleClass('hidden');
-        }
+        mod.overlay.hideOverlay();
       }
 
 } catch (err) {
@@ -114,51 +135,7 @@ alert("error: " + err);
     });
 
 
-    //
-    // move into advanced menu
-    //
-    try {
-      if (document.getElementById("game-wizard-advanced-btn")) {
-	let advbtn = document.getElementById("game-wizard-advanced-btn");
-	if (advbtn) {
-	  advbtn.onclick = (e) => {
 
-	    let gwmain = document.getElementById("game-wizard-main");
-	    let gwside = document.getElementById("game-wizard-sidebar");
-	    let gwadv  = document.getElementById("game-wizard-advanced-options");
-	    let gwadbx = document.getElementById("game-wizard-advanced-box");
-
-	    gwmain.style.display = "none";
-	    gwside.style.display = "none";
-	    gwadv.style.display  = "block";
- 	    gwadbx.style.display = "block";
-  	  }
-	}
-      }
-    } catch (err) {}
-
-
-
-    //
-    // move into advanced menu
-    //
-    try {
-      if (document.getElementById("game-wizard-advanced-return-btn")) {
-	let advbtn = document.getElementById("game-wizard-advanced-return-btn");
-	if (advbtn) {
-	  advbtn.onclick = (e) => {
-	    let gwmain = document.getElementById("game-wizard-main");
-	    let gwside = document.getElementById("game-wizard-sidebar");
-	    let gwadv  = document.getElementById("game-wizard-advanced-options");
-	    let gwadbx = document.getElementById("game-wizard-advanced-box");
-	    gwadv.style.display  = "none";
- 	    gwadbx.style.display = "none";
-	    gwmain.style.display = "block";
-	    gwside.style.display = "block";
-  	  }
-	}
-      }
-    } catch (err) {}
 
   },
 }
