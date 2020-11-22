4beafe29fd60b72a08dfc27eeec421d6a00b4208
clay <claytonrabenda@gmail.com>
2020-11-22 17:20:29 +0800

    saving progress....

---
+++ mods/saitocrypto/saitocrypto.js
@@ -0,0 +1,113 @@
+const saito = require('../../lib/saito/saito');
+const ModTemplate = require('../../lib/templates/modtemplate');
+const { Keyring }  = require('@polkadot/keyring');
+const { mnemonicGenerate } = require('@polkadot/util-crypto');
+const { ApiPromise, WsProvider }  = require('@polkadot/api');
+const { randomBytes }   = require('crypto');
+class SaitoCrypto extends ModTemplate {
+
+  constructor(app) {
+
+    super(app);
+
+    this.name = "SaitoCrypto";
+    this.description = "Payment Gateway for DOT cryptocurrency in Saito Application";
+    this.categories = "Crptocurrency";
+    this.ticker = "SAITO";
+    this.mods = [];
+  }
+  
+  requestInterface(type = "") {
+    if (type == "___is_cryptocurrency") {
+      return {
+        // The ticker of the cryptocurrency
+        ticker: this.ticker,
+        // get balance of given address.
+        //  getBalance(address)
+        getBalance: () => { return 0; },
+        // transfer to another addresses
+        transfer: null,
+        // sign and send a raw transaction
+        signAndSend: null,
+        subscribe: null,
+        getPubkey: () => { return "asdf"; },
+        getKeyring: null,
+        buildRawTx: null,
+      }
+    }
+    return null;
+  }
+  
+  
+  // const unsub = await api.query.system.account(ADDR, ({ nonce, data: balance }) => {
+  //   console.log(`free balance is ${balance.free} with ${balance.reserved} reserved and a nonce of ${nonce}`);
+  // });
+  
+  async getBal(){
+    
+    console.log("getbalanace");
+    //const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+    //const wsProvider = new WsProvider('wss://localhost:8888');
+    //const wsProvider = new WsProvider('ws://localhost:8887');
+    const wsProvider = new WsProvider('ws://localhost');
+    //const wsProvider = new WsProvider('ws://138.197.202.211');
+    
+    console.log("made provider");
+    // returns an API instance when connected, decorated and ready-to use...
+    const api = await ApiPromise.create({ provider: wsProvider });  
+    console.log("made api");
+    await api.isReady;
+    
+    
+    let ADDR = '5DTestUPts3kjeXSTMyerHihn1uwMfLj8vU8sqF7qYrFabHE';
+    ADDR = "13MfMQeGbBb73HAAGRctG8GomWg3mXumP2vnrrRqxBBbqiQH";
+    console.log("isReady");
+    setInterval(async() => {
+      // Retrieve the last timestamp
+      const now = await api.query.timestamp.now();
+      
+      // Retrieve the account balance & nonce via the system module
+      const { nonce, data: balance } = await api.query.system.account(ADDR);
+      
+      console.log(`${now}: balance of ${balance.free} and a nonce of ${nonce}`);  
+    }, 2000);
+    
+    
+    
+  }
+  initialize(app) {
+
+    super.initialize(app);
+  }
+
+  initializeHTML(app) {
+    if (this.app.BROWSER == 0 || !this.browser_active) { return; };
+    document.querySelector("#content .main").innerHTML = "<div id='greeting'>Hello World!</div>"
+    addCss();
+  }
+  tryLoadFromLocalStorage() {
+    
+  }
+  getBalance() {
+    
+  }
+  // createAccount(seed)
+  // create
+  
+  // User has an "account" on the system, a multisig wallet
+  // They can request to start a game with 50% of the funds in the wallet
+  // When funds are "in play", other modules must be restricted from using those funds as collateral for other games
+  //
+  // createMultisigWallet
+  // 
+  // useMultisigWallet
+  // hasMultisigWallet
+  // isFunded
+  // sendToPlayer
+  // 
+  
+
+}
+
+module.exports = SaitoCrypto;
+
---
+++ mods/superwallet/superwallet.js
@@ -0,0 +1,72 @@
+const saito = require('./../../lib/saito/saito');
+const ModTemplate = require('../../lib/templates/modtemplate');
+
+class SuperWallet extends ModTemplate {
+
+  constructor(app) {
+
+    super(app);
+
+    this.name = "SuperWallet";
+    this.description = "Interface for creating and joining games coded for the Saito Open Source Game Engine.";
+    this.categories = "Games Entertainment";
+    this.default_html = 1;
+  }
+  
+  requestInterface(type = "", interfaceBuilder = null) {
+    return null;
+  }
+  
+  initialize(app) {
+    console.log("superwallet init");
+    super.initialize(app);
+
+  }
+  
+  async render(app) {
+    console.log("superwallet render");
+    if (!document.getElementById("superwallet-container")) { 
+      app.browser.addElementToDom('<div id="superwallet-container" class="superwallet-container"></div>'); 
+    }
+    app.modules.requestInterfaces("is_cryptocurrency").forEach(async(response, i) => {
+      console.log("is crypto");
+      let pubkey = await response.getPubkey();
+      console.log("got pubkey");
+      let balance = await response.getBalance();
+      console.log("got balance");
+      app.browser.addElementToDom(`<div>
+        <div>${response.ticker}</div>
+        <div>${pubkey}</div>
+        <div>${balance}</div>
+      </div>`, "arcade-container");
+      
+    });
+
+  }
+
+  // async onConfirmation(blk, tx, conf, app) {
+  // 
+  // }
+
+  // webServer(app, expressapp, express) {
+  //   super.webServer(app, expressapp, express);
+  //   expressapp.get('/arcade/observer/:game_id', async (req, res) => {
+  //     if (games.length > 0) {
+  //       let game = games[0];
+  //       res.setHeader('Content-type', 'text/html');
+  //       res.charset = 'UTF-8';
+  //       //console.info(JSON.stringify(game));
+  //       res.write(game.game_state);
+  //       res.end();
+  //       return;
+  //     }
+  //   });
+  // }
+
+
+
+
+}
+
+module.exports = SuperWallet;
+
