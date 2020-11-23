9b1162872e5915501d6dbfc0a04affadb43c5e7c
clay <claytonrabenda@gmail.com>
2020-11-23 16:39:56 +0800

    progress on cryptocurrency integration

--- mods/ksmcrypto/ksmcrypto.js
+++ mods/ksmcrypto/ksmcrypto.js
@@ -4,16 +4,16 @@ const { Keyring }  = require('@polkadot/keyring');
 const { mnemonicGenerate } = require('@polkadot/util-crypto');
 const { ApiPromise, WsProvider }  = require('@polkadot/api');
 const { randomBytes }   = require('crypto');
-class DOTCrypto extends ModTemplate {
+class KSMCrypto extends ModTemplate {
 
   constructor(app) {
 
     super(app);
 
-    this.name = "DOTCrypto";
+    this.name = "KSMCrypto";
     this.description = "Payment Gateway for DOT cryptocurrency in Saito Application";
     this.categories = "Crptocurrency";
-    this.ticker = "DOT";
+    this.ticker = "KSM";
     this.optionsStorage = {};
     this.app = app;
     this._api = null; // treat as private, please use getApi to access
@@ -35,37 +35,22 @@ class DOTCrypto extends ModTemplate {
     return null;
   }
   async initializeApi() { 
-    console.log("initializeApi");
-    const wsProvider = new WsProvider('wss://rpc.polkadot.io');
-    //const wsProvider = new WsProvider('wss://localhost:8888');
-    //const wsProvider = new WsProvider('ws://localhost:8887');
-    //const wsProvider = new WsProvider('ws://localhost');
-    //const wsProvider = new WsProvider('ws://138.197.202.211');
-    
-    console.log("made provider");
-    // returns an API instance when connected, decorated and ready-to use...
-    //const api = await ApiPromise.create({ provider: wsProvider });  
-    //this._api = await ApiPromise.create({ provider: wsProvider });  
+    const wsProvider = new WsProvider('wss://kusama-rpc.polkadot.io/')
     this._api = new ApiPromise({ provider: wsProvider });
-    console.log("made api");
     await this._api.isReady;
-    console.log("api ready...");
   }
   async getApi() {
-    console.log("getapi");
-    console.log(this._api);
+    console.log("KSMCrypto getapi");
     await this._api.isReady;
-    console.log("ready...");
+    console.log("KSMCrypto ready...");
     return this._api;
   }
   async getPubkey() {
-    return this.optionsStorage.keypair3.address;
+    return this.optionsStorage.keypair.address;
   }
   async getBal(){
-    console.log("getbalance");
     let api = await this.getApi();
-    console.log("got api");
-    const { nonce, data: balance } = await api.query.system.account(this.optionsStorage.keypair3.address);
+    const { nonce, data: balance } = await api.query.system.account(this.optionsStorage.keypair.address);
     return balance.free;  
   }
   async transfer(howMuch, to) {
@@ -95,57 +80,30 @@ class DOTCrypto extends ModTemplate {
     }
   }
   initialize(app) {
-    console.log("****************** DOTCrypto initialize ******************");
+    console.log("****************** KSMCrypto initialize ******************");
     if(app.BROWSER) {
       super.initialize(app);
-      const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+      const provider = new WsProvider('wss://kusama-rpc.polkadot.io/');
       this._api = new ApiPromise({ provider: wsProvider });
       this.getApi();
       this.load();
-        // sr25519 only has a WASM interface
+      // sr25519 only has a WASM interface
       const keyring = new Keyring({ type: 'ed25519', ss58Format: 2 });
       if(!this.optionsStorage.keypair) {
         this.optionsStorage.keypair = keyring.addFromSeed(randomBytes(32), { name: 'polkadot pair' }, 'ed25519');
         this.save();
+      } else {
+        this.optionsStorage.keypair = keyring.addFromJson(JSON.parse('{"address":"5ERZ1oCunsuRueffZBEfsScu169GReLHb84yaETzMQZcsjko","encoded":"4G+yKKzNBkVqGDh9TCcK7UJl6Xjn+l3q98RYrpzHakkAgAAAAQAAAAgAAADy8d3T+iSCaH4EjSwHU3tGBmbPOUGL3WI8eII/vt466R3k3Kc7bmSpIDutifmRg8gE2ay0uFie+iMpTZ/5U6KHi7jlo1NZ6nlxZSQBC4w1tBfCd3QfPA7usxrtnUGDVngL+WvbvJlh0nwWU8CKVJptRjtNrjv5QXhOLTcpdO8dmshN2DbJVWtn7iaalaLoB6ngzMhK1kGV76DTmp7j","encoding":{"content":["pkcs8","sr25519"],"type":["scrypt","xsalsa20-poly1305"],"version":"3"},"meta":{"genesisHash":null,"name":"saito","whenCreated":1606092444131}}'));
+        this.save();
       }
     }
   }
-
   initializeHTML(app) {
     if (this.app.BROWSER == 0 || !this.browser_active) { return; };
     document.querySelector("#content .main").innerHTML = "<div id='greeting'>Hello World!</div>"
     addCss();
   }
-  tryLoadFromLocalStorage() {
-    
-  }
-  getBalance() {
-    
-  }
-  
-  
-  // const unsub = await api.query.system.account(ADDR, ({ nonce, data: balance }) => {
-  //   console.log(`free balance is ${balance.free} with ${balance.reserved} reserved and a nonce of ${nonce}`);
-  // });
-  
-  
-  // createAccount(seed)
-  // create
-  
-  // User has an "account" on the system, a multisig wallet
-  // They can request to start a game with 50% of the funds in the wallet
-  // When funds are "in play", other modules must be restricted from using those funds as collateral for other games
-  //
-  // createMultisigWallet
-  // 
-  // useMultisigWallet
-  // hasMultisigWallet
-  // isFunded
-  // sendToPlayer
-  // 
-  
-
 }
 
-module.exports = DOTCrypto;
+module.exports = KSMCrypto;
 
