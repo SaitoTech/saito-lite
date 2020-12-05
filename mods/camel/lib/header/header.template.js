7c20a08935ee2a1e24c40c9371c13ac789bfce5e
Richard Parris <github@hda0.net>
2020-12-05 14:41:43 +0800

    innohub updates

--- mods/camel/lib/header/header.template.js
+++ mods/camel/lib/header/header.template.js
@@ -5,6 +5,7 @@ module.exports = HeaderTemplate = (ddmods) => {
       <img class="logo powered-by" src="/camel/img/dreamscape-logo.png" />
     </a>
       <i id="settings" class="header-icon icon-med fas fa-cog"></i>
+      <i id="scan-now" class="header-icon icon-med fas fa-qrcode"></i>
 
   <div style="display:none"><input id="settings-restore-account" class="settings-restore-account" type="file" /></div>
 
