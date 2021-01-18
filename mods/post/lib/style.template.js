e0501efa19211ccdfaa7f728a7e2a40802538727
David Lancashire <david@saito.tech>
2021-01-18 23:58:03 +0400

    forum font size

--- mods/post/lib/style.template.js
+++ mods/post/lib/style.template.js
@@ -223,7 +223,7 @@ module.exports = PostStyleTemplate = (app, mod) => {
   overflow-y: scroll;
   border-radius: 3px;
   box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.75);
-  font-size: 1.5em;
+  font-size: 1.15em;
 }
 
 .post-view-overview {}
@@ -237,6 +237,9 @@ module.exports = PostStyleTemplate = (app, mod) => {
   float: left;
   font-size: 1.2em;
   font-weight: bold;
+}
+.post-view-title a {
+  text-decoration: none;
 }
 
 .post-view-sublinks {
