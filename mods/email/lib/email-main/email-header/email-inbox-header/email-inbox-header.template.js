aca54b499ae883edce331823eed709c855ef1ddd
Clayton Rabenda <clay@saito.tech>
2021-02-15 17:44:48 +0800

    email header css fixes

--- mods/email/lib/email-main/email-header/email-inbox-header/email-inbox-header.template.js
+++ mods/email/lib/email-main/email-header/email-inbox-header/email-inbox-header.template.js
@@ -1,13 +1,13 @@
 module.exports = EmailInboxHeaderTemplate = (app, mod, boxname) => {
   return `
     <div class="email-inbox-header-holder">
-      <div class="email-box-name">${boxname}</div>
-      <div class="email-balance"><span id="email-balance"></span><nbsp/><span id="email-token"></span></div>
       <div class="email-icons">
         <input id="email-select-icon" type="checkbox">
         <i id="email-delete-icon" class="icon-med far fa-trash-alt"></i>
         <i id="email-bars-icon" class="icon-med fas fa-bars"></i>
       </div>
+      <div class="email-balance"><span id="email-balance"></span><nbsp/><span id="email-token"></span></div>
+      <div class="email-box-name">${boxname}</div>
     </div>
   `;
 }
