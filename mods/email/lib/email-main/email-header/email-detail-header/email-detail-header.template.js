51c41a536dee87f008df9ebe0ae99c47355b2279
Clayton Rabenda <clay@saito.tech>
2021-02-25 12:18:23 +0800

    wallet header fixes and other css fixes

--- mods/email/lib/email-main/email-header/email-detail-header/email-detail-header.template.js
+++ mods/email/lib/email-main/email-header/email-detail-header/email-detail-header.template.js
@@ -5,13 +5,13 @@ module.exports = EmailDetailHeaderTempkate = (app, mod) => {
       <i id="email-form-back-button" class="icon-med fas fa-arrow-left"></i>
     </div>
     <div class="email-detail-right-options">
-      <div class="email-icons">
+      <div class="email-icons-detail">
         <i id="email-delete-icon" class="icon-med far fa-trash-alt"></i>
         <i id="email-detail-reply" class="icon-med fas fa-reply"></i>
         <i id="email-detail-forward" class="icon-med fas fa-share"></i>
         <i id="email-bars-icon" class="icon-med fas fa-bars"></i>
       </div>
-      <div class="email-balance"><span id="email-balance"></span><nbsp/><span id="email-token"></span></div>
+      <div class="email-balance-detail"><span id="email-balance"></span><nbsp/><span id="email-token"></span></div>
     </div>
   </div>
   `;
