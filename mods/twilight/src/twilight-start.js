7ae0b6ca382cbd6a8d90bad1ee95adad51cf896b
David Lancashire <david@saito.tech>
2021-02-25 13:38:26 +0400

    turning off testing mode in TS

--- mods/twilight/src/twilight-start.js
+++ mods/twilight/src/twilight-start.js
@@ -46,7 +46,7 @@ class Twilight extends GameTemplate {
 
     this.moves           = [];
     this.cards    	 = [];
-    this.is_testing 	 = 1;
+    this.is_testing 	 = 0;
 
     //
     // newbie mode
