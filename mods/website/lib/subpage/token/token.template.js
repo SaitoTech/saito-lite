af3999a45e26b045d5c5638f4c3a8cae6af6571d
Clayton Rabenda <clay@saito.tech>
2021-04-20 13:33:50 +0800

    copy changes from YC

--- mods/website/lib/subpage/token/token.template.js
+++ mods/website/lib/subpage/token/token.template.js
@@ -2,12 +2,12 @@ module.exports = TokenTemplate = () => {
   
   let content = {
     title: "SAITO's token is launching in less than 48 hours",
-    para1: "Our Polkastarter pool participants are KYCing now and the pool will open on April 22, 1PM UTC.",
-    para2: "We will also be announcing the token address and creating a Uniswap pool directly following the IDO, so keep tuned to saito.io for more information as we proceed toward IDO day.",
-    para3: "All private sale participants who are interested in participating in the ERC-20 and have completed KYC will also receive their tokens immediately following the IDO.",
+    para1: "Our Polkastarter pool participants are KYC-ing now and the pool will open on April 22, 1PM UTC.",
+    para2: "We will also be announcing the token address and creating a Uniswap pool directly following the IDO, so stay tuned to saito.io for more information as we proceed toward IDO day.",
+    para3: "All private sale participants will also receive their tokens immediately following the IDO, subject to their vesting terms.",
     para4: "Thanks to everyone in the community for your enthusiatic support.",
   }
-  if(document.location.host.includes("cn.saito.io")){
+  if(document.location.host.includes("cn.saito.io") || true){
     content = {
       title: "SAITO代币将在未来48小时内上线。",
       para1: "我们的PolkaStarter Pool参与者现在正在进行KYC中，该池将于4月22日UTC下午1点开放。",
