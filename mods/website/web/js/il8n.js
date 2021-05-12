c6127edf10038cf753e23ccc6a3de9b8e0b7fb61
MelimeliDev <melisa.surja.pro@gmail.com>
2021-05-12 14:57:45 +0800

    review fixes, landing page footer layout fix, change language btn toggle

--- mods/website/web/js/il8n.js
+++ mods/website/web/js/il8n.js
@@ -4,7 +4,6 @@
 var il8n_file = null;
 
 const il8n = (targetLang) => {
-    document.querySelector("html").setAttribute("lang", targetLang);
     // Replace all text with "il8n" class to locale
     document.querySelectorAll("[data-il8n]").forEach(el => {
         const selector = el.getAttribute("data-il8n");
@@ -12,28 +11,47 @@ const il8n = (targetLang) => {
         if (il8n_file.hasOwnProperty(selector) && il8n_file[selector].hasOwnProperty(targetLang) && (il8n_file[selector][targetLang] != ""))
             el.textContent = il8n_file[selector][targetLang];
     });
-    
-    // Change the text to translate to English
-    // document.querySelectorAll(".translate-cn").forEach(el => {
-    //     el.classList.remove("translate-cn");
-    //     el.classList.add("translate-en");
-
-    // });
 };
+
+const toggleBtn = (targetLang = null) => {
+    const currentLang = document.querySelector("html").getAttribute("lang");
+    if (targetLang == null) {
+        targetLang = (currentLang == "en") ? "zh" : "en";
+    }
+    if (currentLang == targetLang) return targetLang;
+
+    document.querySelector("html").setAttribute("lang", targetLang);
+
+    // change text to targetLang
+    document.querySelectorAll(".translate-toggle").forEach(el=> {
+        const from = el.getAttribute("data-il8n-from");
+        if (targetLang == from) {
+            const toText = el.getAttribute("data-il8n-to-text");
+            el.textContent = toText;
+        } else {
+            const fromText = el.getAttribute("data-il8n-from-text");
+            el.textContent = fromText;
+        }
+    });
+
+    return targetLang;
+}
 
 document.onreadystatechange = async function () {
     if (document.readyState === "interactive") {
         il8n_file = await fetch(`/website/il8n/homepage.json`)
         .then(data => data.json())
-        .then(data => data)
         .catch(err => {});
 
-        il8n("en");
-
-        document.querySelectorAll(".translate-cn").forEach(el=> {
+        if (document.location.host.includes("cn.saito.io"))
+            il8n("zh");
+        else
+            il8n("en");
+        document.querySelectorAll(".translate-toggle").forEach(el=> {
             el.addEventListener("click", e => {
                 e.preventDefault();
-                il8n("cn");
+                const targetLang = toggleBtn();
+                il8n(targetLang);
             });
         });
     }
