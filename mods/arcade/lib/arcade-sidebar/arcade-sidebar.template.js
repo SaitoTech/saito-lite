2e6a0b664ee2fc241185c50de6242f94b11cad22
David Lancashire <david.lancashire@gmail.com>
2021-06-06 14:01:35 +0700

    add scheduled games announcements to arcade

--- mods/arcade/lib/arcade-sidebar/arcade-sidebar.template.js
+++ mods/arcade/lib/arcade-sidebar/arcade-sidebar.template.js
@@ -14,6 +14,7 @@ module.exports = ArcadeSidebarTemplate = () => {
         </div>
     </div>
   </div>
+  <div id="arcade-announcement" class="arcade-announcement">Like large multiplayer games? See our upcoming <a href="https://docs.google.com/spreadsheets/d/1f1QrDFd8f6RaIJhV5m-pQFBoAO9WZGxpCoJmdKBaQwI/edit#gid=0" target="_community">Community Games and Events</a>.</div>
   <div id="email-chat" class="email-chat"></div>
   `;
 }
