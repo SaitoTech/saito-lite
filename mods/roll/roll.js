60a4fbc6099d1028d7ff23d07c85bd10c7a38634
David Lancashire <david.lancashire@gmail.com>
2021-10-10 23:14:53 +0700

    feat: updates to simplify game engine and UI actions, including HUD and card events

--- mods/roll/roll.js
+++ mods/roll/roll.js
@@ -17,6 +17,9 @@ class Roll extends GameTemplate {
     this.type            = "Classic Cardgame";
     this.card_img_dir = '/roll/img/cards';
 
+    // disable by default
+    this.useHUD = 0;
+
     this.minPlayers = 2;
     this.maxPlayers = 4;
 
@@ -102,56 +105,144 @@ class Roll extends GameTemplate {
       mod.respondTo('chat-manager').attachEvents(app, this);
     });
 
-    //
-    // ADD MENU
-    //
-    this.menu.addMenuOption({
-      text : "Game",
-      id : "game-game",
-      class : "game-game",
-      callback : function(app, game_mod) {
-        game_mod.menu.showSubMenu("game-game");
-      }
-    });
-    this.menu.addSubMenuOption("game-game", {
-      text : "Log",
-      id : "game-log",
-      class : "game-log",
-      callback : function(app, game_mod) {
-        game_mod.menu.hideSubMenus();
-        game_mod.log.toggleLog();
-      }
-    });
-    this.menu.addSubMenuOption("game-game", {
-      text : "Exit",
-      id : "game-exit",
-      class : "game-exit",
-      callback : function(app, game_mod) {
-        window.location.href = "/arcade";
-      }
-    });
-    this.menu.render(app, this);
-    this.menu.attachEvents(app, this);
 
     //
-    // we want hud to support cardbox, so re-render
+    // if we support cardbox, we have to re-render the hud after
+    // it has already been initiatized.
     //
-    this.hud.render(app, this);
-    this.hud.attachEvents(app, this);
-
-
-console.log("QUEUE: " + JSON.stringify(this.game.queue));
+    //this.hud.render(app, this);
+    //this.hud.attachEvents(app, this);
 
 
     //
-    // initialize secure roll
+    // make the log visible
     //
+    this.log.toggleLog();
+
     let game_self = this;
+
     try {
 
+      //
+      // update interface with active crypto
+      //
       if (game_self.game.crypto != "") {
         document.getElementById("saito_crypto").innerHTML = game_self.game.crypto;
       }
+
+      //
+      // secure dice roll
+      //
+      document.getElementById("secure_dice_roll_button").onclick = (e) => {
+
+	game_self.updateLog(`
+
+			Calling the function requestSecureRoll() prior to submitting 
+			a move will initiate a cryptographically-secure re-generation 
+			of the random number used to generate dice rolls. This prevents 
+			any player from being able to pre-calculate. The mechanism 
+		     	implemented requires all players to contribute randomization to 
+			the number.
+
+			    `);
+        game_self.requestSecureRoll();
+        game_self.endTurn();
+      }
+
+
+      //
+      // consecutive moves
+      //
+      document.getElementById("consecutive_moves_button").onclick = (e) => {
+
+	game_self.updateLog(`
+
+			This illustrates how to get both players moving one-by-one. First
+			one player will move. And when they have completed their move the
+			other player will be asked to move.
+
+			    `);
+	for (let i = 0; i < game_self.game.players.length; i++) {
+	  game_self.addMove("NOTIFY\tPlayer "+(i+1)+" is finished moving");
+	  game_self.addMove("PLAY\t"+(i+1))
+	  game_self.addMove("NOTIFY\tPlayer "+(i+1)+" is about to move");
+	}
+        game_self.endTurn();
+
+      }
+
+
+      //
+      // simultaneous moves
+      //
+      document.getElementById("simultaneous_moves_button").onclick = (e) => {
+
+	game_self.updateLog(`
+
+			This illustrates how to provide the opportunity for multiple players
+			to perform actions at the same time. Note that these are not blind 
+			actions so any player that moves first will have their move visible
+			to others. But it allows for rapid responses to gameplay action in 
+			particular confirming that actions have happened.
+
+			Games should not be designed with nested simultaneous moves. If you 
+			require complicated actions that require responses within these sub-
+			moves it is better to switch to concurrent processing or (better yet)
+			to break gameplay into two steps.
+
+			    `);
+	let players_to_go = [];
+	for (let i = 0; i < game_self.game.players.length; i++) {
+	  players_to_go.push(i+1);
+	}
+	game_self.addMove("NOTIFY\tAll players have finished moving");
+	game_self.addMove("PLAY\t"+JSON.stringify(players_to_go));
+	game_self.addMove("NOTIFY\tAll players will move simultaneously");
+        game_self.endTurn();
+
+      }
+
+
+      //
+      // show non-blocking overlay
+      //
+      document.getElementById("display_overlay_button").onclick = (e) => {
+
+	let overlay_html = `
+	  <div style="background-color:whitesmoke;width:80vw;padding:40px;font-size:1.2em;">
+	  Non-blocking overlays can be closed by clicking on backdrops.
+	  <p></p>
+	  Width and height determined based on content put into overlay.
+	  </div>
+	`;
+
+	this.overlay.showOverlay(this.app, this, overlay_html, function() {
+	  alert("Callback Optional on Close!");
+	});
+
+      }
+
+
+      //
+      // show blocking overlay
+      //
+      document.getElementById("display_blocking_overlay_button").onclick = (e) => {
+
+	let overlay_html = `
+	  <div style="background-color:whitesmoke;width:80vw;padding:40px;font-size:1.2em;">
+	  Blocking overlays cannot be closed by clicking on backdrops.
+	  <p></p>
+	  <div class="button close_overlay_button" id="close_overlay_button">close overlay</div>
+	  Width and height determined based on content put into overlay.
+	  </div>
+	`;
+
+	this.overlay.showOverlayBlocking(this.app, this, overlay_html);
+	document.getElementById("close_overlay_button").onclick = (e) => { game_self.overlay.hideOverlay(); }
+
+      }
+
+
 
       //
       // secure dice roll
@@ -206,9 +297,6 @@ console.log("QUEUE: " + JSON.stringify(this.game.queue));
       //
       document.getElementById("deal_cards_to_table_button").onclick = (e) => {
 
-	//
-	// DECK / DECKENCRYPT / DECKXOR 
-	//
 	game_self.updateLog(`
 
 			dealing cards to a common pool requires creating and encrypting
@@ -283,6 +371,7 @@ console.log("QUEUE: " + JSON.stringify(this.game.queue));
       //
       document.getElementById("check_balance_button").onclick = (e) => {
 
+	let amount = 0;
 	let address = game_self.game.keys[game_self.game.player-1];
 	let ticker = game_self.game.crypto;
 
@@ -290,31 +379,91 @@ console.log("QUEUE: " + JSON.stringify(this.game.queue));
 	game_self.updateLog(`The game engine is checking to see if the balance at address ${address} is at least ${amount} ${ticker}. The game will halt for all players until the balance is at this amount.`);
 
         if (ticker != "") {
-	  game_self.addMove("BALANCE" + "\t" + "0" + "\t" + address + "\t" + ticker);
+	  game_self.addMove("BALANCE" + "\t" + amount + "\t" + address + "\t" + ticker);
         } else {
-	  game_self.addMove("BALANCE" + "\t" + "0" + "\t" + address);
+	  game_self.addMove("BALANCE" + "\t" + amount + "\t" + address);
 	}
 	game_self.endTurn();
 
       }
 
+
+      //
+      // display cardfan
+      //
+      document.getElementById("display_cardfan_button").onclick = (e) => {
+        this.cardfan.render(this.app, this);
+        this.cardfan.attachEvents(this.app, this);
+      }
+
+
+      //
+      // display cardhud
+      //
+      document.getElementById("display_cardhud_button").onclick = (e) => {
+        this.hud.render(this.app, this);
+        this.hud.attachEvents(this.app, this);
+	this.hud.updateStatusMessageAndShowCards('updating the status message', this.game.deck[0].hand, this.game.deck[0].cards);
+	this.hud.onCardClick(function(card) {
+	  alert("Selected a card: "+card);
+	});
+      }
+
+
+      //
+      // toggle cardbox
+      //
+      document.getElementById("toggle_cardbox_button").onclick = (e) => {
+        this.hud.render(this.app, this);
+        this.hud.attachEvents(this.app, this);
+        if (this.hud.use_cardbox == 1) {
+	  this.hud.use_cardbox = 0;
+	  this.hud.updateStatusMessageAndShowCards('cardbox disabled in hud', this.game.deck[0].hand, this.game.deck[0].cards);
+	} else {
+	  this.hud.use_cardbox = 1;
+	  this.hud.updateStatusMessageAndShowCards('cardbox enabled in hud', this.game.deck[0].hand, this.game.deck[0].cards);
+	}
+      }
+
+
+      //
+      // add to menu to page
+      //
+      document.getElementById("add_menu_button").onclick = (e) => {
+        this.menu.addMenuOption({
+          text : "Game",
+          id : "game-game",
+          class : "game-game",
+          callback : function(app, game_mod) {
+            game_mod.menu.showSubMenu("game-game");
+          },
+        });
+        this.menu.addSubMenuOption("game-game", {
+          text : "Log",
+          id : "game-log",
+          class : "game-log",
+          callback : function(app, game_mod) {
+            game_mod.menu.hideSubMenus();
+            game_mod.log.toggleLog();
+          },
+        });
+        this.menu.addSubMenuOption("game-game", {
+          text : "Exit",
+          id : "game-exit",
+          class : "game-exit",
+          callback : function(app, game_mod) {
+            window.location.href = "/arcade";
+          },
+        });
+        this.menu.render(this.app, this);
+        this.menu.attachEvents(this.app, this);
+      }
+
     } catch (err) {
+console.log("error: " + err);
     }
-
-
-
-
-
 }
-/****
 
-        <div class="button simultaneous_moves" id="simultaneous_moves_button">Simultaneous Moves</div>
-
-        <p></p>
-
-        <div class="button shuffle_cards" id="shuffle_cards">Shuffle Cards</div>
-
-****/
 
 
 
@@ -330,8 +479,6 @@ console.log("QUEUE: " + JSON.stringify(this.game.queue));
     // QUEUE //
     ///////////
     if (this.game.queue.length > 0) {
-
-console.log("QUEUE: " + JSON.stringify(this.game.queue));
 
       let qe = this.game.queue.length - 1;
       let mv = this.game.queue[qe].split("\t");
@@ -350,16 +497,11 @@ console.log("QUEUE: " + JSON.stringify(this.game.queue));
 
     }
 
-
     return 1;
   }
 
   playerPlay(player_id) {
-
     if (player_id == this.game.player) {
-
-
-
     }
   }
 
@@ -383,61 +525,66 @@ console.log("QUEUE: " + JSON.stringify(this.game.queue));
 
     var deck = {};
 
-    deck['1'] = { name: "S1.png" }
-    deck['2'] = { name: "S2.png" }
-    deck['3'] = { name: "S3.png" }
-    deck['4'] = { name: "S4.png" }
-    deck['5'] = { name: "S5.png" }
-    deck['6'] = { name: "S6.png" }
-    deck['7'] = { name: "S7.png" }
-    deck['8'] = { name: "S8.png" }
-    deck['9'] = { name: "S9.png" }
-    deck['10'] = { name: "S10.png" }
-    deck['11'] = { name: "S11.png" }
-    deck['12'] = { name: "S12.png" }
-    deck['13'] = { name: "S13.png" }
-    deck['14'] = { name: "C1.png" }
-    deck['15'] = { name: "C2.png" }
-    deck['16'] = { name: "C3.png" }
-    deck['17'] = { name: "C4.png" }
-    deck['18'] = { name: "C5.png" }
-    deck['19'] = { name: "C6.png" }
-    deck['20'] = { name: "C7.png" }
-    deck['21'] = { name: "C8.png" }
-    deck['22'] = { name: "C9.png" }
-    deck['23'] = { name: "C10.png" }
-    deck['24'] = { name: "C11.png" }
-    deck['25'] = { name: "C12.png" }
-    deck['26'] = { name: "C13.png" }
-    deck['27'] = { name: "H1.png" }
-    deck['28'] = { name: "H2.png" }
-    deck['29'] = { name: "H3.png" }
-    deck['30'] = { name: "H4.png" }
-    deck['31'] = { name: "H5.png" }
-    deck['32'] = { name: "H6.png" }
-    deck['33'] = { name: "H7.png" }
-    deck['34'] = { name: "H8.png" }
-    deck['35'] = { name: "H9.png" }
-    deck['36'] = { name: "H10.png" }
-    deck['37'] = { name: "H11.png" }
-    deck['38'] = { name: "H12.png" }
-    deck['39'] = { name: "H13.png" }
-    deck['40'] = { name: "D1.png" }
-    deck['41'] = { name: "D2.png" }
-    deck['42'] = { name: "D3.png" }
-    deck['43'] = { name: "D4.png" }
-    deck['44'] = { name: "D5.png" }
-    deck['45'] = { name: "D6.png" }
-    deck['46'] = { name: "D7.png" }
-    deck['47'] = { name: "D8.png" }
-    deck['48'] = { name: "D9.png" }
-    deck['49'] = { name: "D10.png" }
-    deck['50'] = { name: "D11.png" }
-    deck['51'] = { name: "D12.png" }
-    deck['52'] = { name: "D13.png" }
+    deck['1'] = { name: "S1.png" , img: "/roll/img/cards/S1.png" }
+    deck['2'] = { name: "S2.png" , img: "/roll/img/cards/S2.png" }
+    deck['3'] = { name: "S3.png" , img: "/roll/img/cards/S3.png" }
+    deck['4'] = { name: "S4.png" , img: "/roll/img/cards/S4.png" }
+    deck['5'] = { name: "S5.png" , img: "/roll/img/cards/S5.png" }
+    deck['6'] = { name: "S6.png" , img: "/roll/img/cards/S6.png" }
+    deck['7'] = { name: "S7.png" , img: "/roll/img/cards/S7.png" }
+    deck['8'] = { name: "S8.png" , img: "/roll/img/cards/S8.png" }
+    deck['9'] = { name: "S9.png" , img: "/roll/img/cards/S9.png" }
+    deck['10'] = { name: "S10.png" , img: "/roll/img/cards/S10.png" }
+    deck['11'] = { name: "S11.png" , img: "/roll/img/cards/S11.png" }
+    deck['12'] = { name: "S12.png" , img: "/roll/img/cards/S12.png" }
+    deck['13'] = { name: "S13.png" , img: "/roll/img/cards/S13.png" } 
+    deck['14'] = { name: "C1.png" , img: "/roll/img/cards/C1.png" }
+    deck['15'] = { name: "C2.png" , img: "/roll/img/cards/C2.png" }
+    deck['16'] = { name: "C3.png" , img: "/roll/img/cards/C3.png" }
+    deck['17'] = { name: "C4.png" , img: "/roll/img/cards/C4.png" }
+    deck['18'] = { name: "C5.png" , img: "/roll/img/cards/C5.png" }
+    deck['19'] = { name: "C6.png" , img: "/roll/img/cards/C6.png" }
+    deck['20'] = { name: "C7.png" , img: "/roll/img/cards/C7.png" }
+    deck['21'] = { name: "C8.png" , img: "/roll/img/cards/C8.png" }
+    deck['22'] = { name: "C9.png" , img: "/roll/img/cards/C9.png" }
+    deck['23'] = { name: "C10.png" , img: "/roll/img/cards/C10.png" }
+    deck['24'] = { name: "C11.png" , img: "/roll/img/cards/C11.png" }
+    deck['25'] = { name: "C12.png" , img: "/roll/img/cards/C12.png" }
+    deck['26'] = { name: "C13.png" , img: "/roll/img/cards/C13.png" }
+    deck['27'] = { name: "H1.png" , img: "/roll/img/cards/H1.png" }
+    deck['28'] = { name: "H2.png" , img: "/roll/img/cards/H2.png" }
+    deck['29'] = { name: "H3.png" , img: "/roll/img/cards/H3.png" }
+    deck['30'] = { name: "H4.png" , img: "/roll/img/cards/H4.png" }
+    deck['31'] = { name: "H5.png" , img: "/roll/img/cards/H5.png" }
+    deck['32'] = { name: "H6.png" , img: "/roll/img/cards/H6.png" }
+    deck['33'] = { name: "H7.png" , img: "/roll/img/cards/H7.png" }
+    deck['34'] = { name: "H8.png" , img: "/roll/img/cards/H8.png" }
+    deck['35'] = { name: "H9.png" , img: "/roll/img/cards/H9.png" }
+    deck['36'] = { name: "H10.png" , img: "/roll/img/cards/H10.png" }
+    deck['37'] = { name: "H11.png" , img: "/roll/img/cards/H11.png" }
+    deck['38'] = { name: "H12.png" , img: "/roll/img/cards/H12.png" }
+    deck['39'] = { name: "H13.png" , img: "/roll/img/cards/H13.png" }
+    deck['40'] = { name: "D1.png" , img: "/roll/img/cards/D1.png" }
+    deck['41'] = { name: "D2.png" , img: "/roll/img/cards/D2.png" }
+    deck['42'] = { name: "D3.png" , img: "/roll/img/cards/D3.png" }
+    deck['43'] = { name: "D4.png" , img: "/roll/img/cards/D4.png" }
+    deck['44'] = { name: "D5.png" , img: "/roll/img/cards/D5.png" }
+    deck['45'] = { name: "D6.png" , img: "/roll/img/cards/D6.png" }
+    deck['46'] = { name: "D7.png" , img: "/roll/img/cards/D7.png" }
+    deck['47'] = { name: "D8.png" , img: "/roll/img/cards/D8.png" }
+    deck['48'] = { name: "D9.png" , img: "/roll/img/cards/D9.png" }
+    deck['49'] = { name: "D10.png" , img: "/roll/img/cards/D10.png" }
+    deck['50'] = { name: "D11.png" , img: "/roll/img/cards/D11.png" }
+    deck['51'] = { name: "D12.png" , img: "/roll/img/cards/D12.png" }
+    deck['52'] = { name: "D13.png" , img: "/roll/img/cards/D13.png" }
 
     return deck;
 
+  }
+
+  addShowCardEvents(onCardClickFunction=null) {
+    this.changeable_callback = onCardClickFunction;
+    this.hud.attachCardEvents(this.app, this);
   }
 
 }
